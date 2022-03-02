d656352119d9f3d372e6d7d40b1014d131ff86ac
Kosu <sebastian@studiograficzne.com>
2022-03-02 12:41:49 +0100

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,72 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('auth.login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+<div class="wrapper">
+    <div class="page-header" style="background-image: url('../img/register_bg.png'), linear-gradient(104.36deg, #00264c 60%, #0054A6 90%);">
+        <div class="filter"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 offset-lg-4 col-sm-6 offset-sm-3">
+                    <div class="card card-register">
+                        <h3 class="card-title">{{ trans('labels.frontend.auth.login_box_title') }}</h3>
+                        {{ Form::open(['route' => 'auth.login', 'class' => 'register-form']) }}
+                        <label for="email">{{ trans('validation.attributes.frontend.register-user.email') }}</label>
+                        <div class="input-group form-group-no-border">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    <i class="nc-icon nc-email-85"></i>
+                                </span>
+                            </div> 
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="{{ trans('validation.attributes.frontend.register-user.email') }}">
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                        <label for="password">{{ trans('validation.attributes.frontend.register-user.password') }}</label>
+                        <div class="input-group form-group-no-border">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    <i class="nc-icon nc-key-25"></i>
+                                </span>
                             </div>
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" value="{{ old('password') }}" required autocomplete="new-password" placeholder="{{ trans('validation.attributes.frontend.register-user.password') }}">
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
+                        <div class="form-check">
+                            <label class="form-check-label">                                
+                                <input id="remember" type="checkbox" class="form-check-input @error('remember') is-invalid @enderror" name="remember" value="0">
+                                <span class="form-check-sign"></span>
+                                <small>{{ trans('labels.frontend.auth.remember_me') }}</small>                                
+                            </label>
                         </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
+                        
+                        {{ Form::submit(trans('labels.frontend.auth.login_button'), ['class' => 'btn btn-primary-blue btn-block btn-round']) }}
+                        {{ Form::close() }}
+                        <div class="forgot">
+                            {{ link_to_route('auth.password.request', trans('labels.frontend.passwords.forgot_password'), [], ['class' => 'btn btn-link btn-primary-blue']) }}
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
+        </div>
+        <div class="footer register-footer text-center">
+            <h6>© 2021, made with <i class="fa fa-heart heart"></i> by RAD</h6>
         </div>
     </div>
 </div>
