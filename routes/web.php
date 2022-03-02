e8cc0f647ba72d885f6e26940617278600eeb35c
Kosu <sebastian@studiograficzne.com>
2022-03-02 12:24:24 +0100

    commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FrontendController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +18,44 @@ use App\Http\Controllers\FrontendController;
 */
 
 Route::get('/', [FrontendController::class, 'index'])->name('welcome');
+
+Route::group(['namespace' => 'Auth', 'as' => 'auth.'], function () {
+
+    /*
+     * These routes require the user to be logged in
+     */
+    Route::group(['middleware' => 'auth'], function () {
+        Route::get('logout', [LoginController::class, 'logout'])->name('logout');
+
+        //For when admin is logged in as user from backend
+        Route::get('logout-as', [LoginController::class, 'logoutAs'])->name('logout-as');
+
+        // Change Password Routes
+        Route::patch('password/change', [ChangePasswordController::class, 'changePassword'])->name('password.change');
+    });
+
+    /*
+     * These routes require no user to be logged in
+     */
+    Route::group(['middleware' => 'guest'], function () {
+        // Authentication Routes
+        Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+        Route::post('login', [LoginController::class, 'login'])->name('login');
+
+        // Socialite Routes
+        //Route::get('login/{provider}', 'SocialLoginController@login')->name('social.login');
+
+        // Registration Routes
+        //if (config('access.users.registration')) {
+        //    Route::get('register', 'RegisterController@showRegistrationForm')->name('register');
+        //    Route::post('register', 'RegisterController@register')->name('register');
+        //}
+
+        // Password Reset Routes
+        Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.email');
+        Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+
+        Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset.form');
+        Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.reset');
+    });
+});
