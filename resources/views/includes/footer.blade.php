8a11cfc22ae5651f23efb8ab6028190693d16b98
Kosu <sebastian@studiograficzne.com>
2022-03-04 13:34:43 +0100

    commit

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="footer section-dark">
+	<div class="container">
+		<div class="row">
+			<nav class="footer-nav">
+				<ul>
+					<li><a href="https://radspzoo.pl">RAD sp. z o.o.</a></li>
+				</ul>
+			</nav>
+			<div class="credits ml-auto">
+				<span class="copyright">© 2022, stworzone z <i class="fa fa-heart heart"></i> przez RAD</span>
+			</div>
+		</div>
+	</div>
+</footer>
