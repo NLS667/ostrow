121cd565a5a5eab5a9f9f8d39be244c353e8a97b
Kosu <sebastian@studiograficzne.com>
2022-03-16 14:18:10 +0100

    commit

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,36 @@ use App\Http\Controllers\Auth\ChangePasswordController;
 
 Route::get('/', [FrontendController::class, 'index'])->name('index');
 
+/*
+ * These frontend controllers require the user to be logged in
+ */
+Route::group(['middleware' => 'auth'], function () {
+    Route::group(['namespace' => 'User', 'as' => 'user.'], function () {
+
+        /*
+         * User Dashboard Specific
+         */
+
+        //Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+        
+        /*
+         * User Account Specific
+         */
+        Route::get('account', [AccountController::class, 'index'])->name('account');
+
+        /*
+         * User Profile Specific
+         */
+        Route::patch('profile/update', [ProfileController::class, 'update'])->name('profile.update');
+
+        /*
+         * User Profile Picture
+         */
+        Route::patch('profile-picture/update', [ProfileController::class, 'updateProfilePicture'])->name('profile-picture.update');
+    });
+});
+
+
 Route::group(['namespace' => 'Auth', 'as' => 'auth.'], function () {
 
     /*
