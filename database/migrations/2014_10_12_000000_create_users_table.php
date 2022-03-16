49e72984facbee28e5a7f4c8ee81e1c5ff4c874c
Kosu <sebastian@studiograficzne.com>
2022-03-16 12:38:25 +0100

    commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,21 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->increments('id');
+            $table->string('first_name', 191);
+            $table->string('last_name', 191);
+            $table->string('email', 191)->unique();
+            $table->date('email_verified_at')->nullable();
+            $table->string('password', 191)->nullable();
+            $table->boolean('status')->default(1);
+            $table->string('confirmation_code', 191)->nullable();
+            $table->boolean('confirmed')->default(0);
+            $table->boolean('is_term_accept')->default(0)->comment(' 0 = not accepted,1 = accepted');
+            $table->string('remember_token', 100)->nullable();
+            $table->integer('created_by')->unsigned();
+            $table->integer('updated_by')->unsigned()->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -31,6 +39,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::drop('users');
     }
 }
