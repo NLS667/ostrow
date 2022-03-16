b511be0c61d70d3cb700c9c52c5d588ac19c47e2
Kosu <sebastian@studiograficzne.com>
2022-03-16 16:03:45 +0100

    commit

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -31,7 +31,7 @@
             @include('backend.includes.sidebar-dynamic')
             
             <!-- Content Wrapper. Contains page content -->
-            <div class="content-wrapper">
+            <div class="content-wrapper main-panel">
                 <!-- Content Header (Page header) -->
                 <div class="content-header">
                     @yield('page-header')
