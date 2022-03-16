01a6160273c454fafb516bb0b67f1e19b9d7691d
Kosu <sebastian@studiograficzne.com>
2022-03-16 18:16:14 +0100

    commit

--- resources/views/backend/includes/sidebar-dynamic.blade.php
+++ resources/views/backend/includes/sidebar-dynamic.blade.php
@@ -13,31 +13,7 @@
                     <p>Pulpit</p>
                 </a>
             </li>
+            {{ renderMenuItems(getMenuItems('backend', 1)) }}
         </ul>
-    </div>
-</div>
-
-<aside class="main-sidebar sidebar-dark-dark elevation-4">
-    <!-- Brand Logo -->
-    <a href="{{ route('admin.index') }}" class="brand-link">
-        <span class="brand-text font-weight-light">OSTR CRM</span>
-    </a>
-
-    <!-- Sidebar -->
-    <div class="sidebar">
-        <!-- Sidebar Menu -->
-        <nav>
-            <ul class="nav nav-sidebar flex-column" data-widget="treeview">
-                <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
-                <li class="nav-header">{{ trans('menus.backend.sidebar.general') }}</li>
-                <li class="nav-item {{ active_class(if_route('admin.index'), 'active', '') }}">
-                    <a href="{{ route('admin.index') }} " class="nav-link">
-                        <i class="fas fa-tachometer-alt nav-icon"></i>
-                        {{ trans('menus.backend.sidebar.dashboard') }}
-                    </a>
-                </li>
-                {{ renderMenuItems(getMenuItems('backend', 1)) }}
-            </ul>
-        </nav><!-- /.sidebar-menu -->
-    </div>
-</aside><!-- /.sidebar -->
+    </div><!-- /.sidebar-menu -->
+</div><!-- /.sidebar -->
