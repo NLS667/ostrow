a7f4522d56cd117b540606498c07e6e11eea8271
Kosu <sebastian@studiograficzne.com>
2022-03-16 15:20:12 +0100

    commit

--- resources/views/backend/includes/nav.blade.php
+++ resources/views/backend/includes/nav.blade.php
@@ -7,7 +7,7 @@
         <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        {{ link_to_route('backend.index','Strona główna', [], ['class' => 'nav-link']) }}
+        {{ link_to_route('admin.index','Strona główna', [], ['class' => 'nav-link']) }}
       </li>
     </ul>
 
