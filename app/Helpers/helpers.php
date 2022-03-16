2d318f17f7a41347d0fb72dd50be7f06b7a2259a
Kosu <sebastian@studiograficzne.com>
2022-03-16 15:23:29 +0100

    commit

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -74,3 +74,53 @@ if (!function_exists('includeRouteFiles')) {
         }
     }
 }
+
+if (!function_exists('renderMenuItems')) {
+    /**
+     * render sidebar menu items after permission check.
+     */
+    function renderMenuItems($items, $viewName = 'backend.includes.partials.sidebar-item')
+    {
+        foreach ($items as $item) {
+            // if(!empty($item->url) && !Route::has($item->url)) {
+            //     return;
+            // }
+            if (!empty($item->view_permission_id)) {
+                if (access()->allow($item->view_permission_id)) {
+                    echo view($viewName, compact('item'));
+                }
+            } else {
+                echo view($viewName, compact('item'));
+            }
+        }
+    }
+}
+
+if (!function_exists('isActiveMenuItem')) {
+    /**
+     * checks if current URL is of current menu/sub-menu.
+     */
+    function isActiveMenuItem($item, $separator = '?')
+    {
+        $item->clean_url = $item->url;
+        if (strpos($item->url, $separator) !== false) {
+            $item->clean_url = explode($separator, $item->url, -1);
+        }
+        if (Active::checkRoutePattern($item->clean_url)) {
+            return true;
+        }
+        if (!empty($item->children)) {
+            foreach ($item->children as $child) {
+                $child->clean_url = $child->url;
+                if (strpos($child->url, $separator) !== false) {
+                    $child->clean_url = explode($separator, $child->url, -1);
+                }
+                if (Active::checkRoutePattern($child->clean_url)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
