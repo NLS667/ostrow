4a9707b37353522f43df1d1c55140d3c3bfe58ca
Kosu <sebastian@studiograficzne.com>
2022-03-16 15:46:05 +0100

    commit

--- config/app.php
+++ config/app.php
@@ -175,8 +175,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\ComposerServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        LaravelFrontendPresets\MaterialPreset\MaterialPresetServiceProvider::class
+        App\Providers\RouteServiceProvider::class
 
     ],
 
