cb20774c31b3d6a8aeb73e4bc5ac508463cf4f2b
Kosu <sebastian@studiograficzne.com>
2022-03-16 13:08:03 +0100

    commit

---
+++ app/Models/ModelTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+trait ModelTrait
+{
+    /**
+     * @return string
+     */
+    public function getEditButtonAttribute($permission, $route)
+    {
+        if (access()->allow($permission)) {
+            return '<a href="'.route($route, $this).'" class="btn btn-flat btn-default">
+                    <i data-toggle="tooltip" data-placement="top" title="Edytuj" class="fas fa-pencil-alt"></i>
+                </a>';
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getDeleteButtonAttribute($permission, $route)
+    {
+        if (access()->allow($permission)) {
+            return '<a href="'.route($route, $this).'" 
+                    class="btn btn-flat btn-default" data-method="delete"
+                    data-trans-button-cancel="Anuluj"
+                    data-trans-button-confirm="Usuń"
+                    data-trans-title="Czy jesteś pewny, że chcesz to zrobić ?">
+                        <i data-toggle="tooltip" data-placement="top" title="Usuń" class="fas fa-trash"></i>
+                </a>';
+        }
+    }
+}
