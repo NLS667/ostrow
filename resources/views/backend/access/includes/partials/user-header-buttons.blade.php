8856cc51e70dd010dc121bde4e31908e8108112f
Kosu <sebastian@studiograficzne.com>
2022-03-17 14:27:27 +0100

    commit

--- resources/views/backend/access/includes/partials/user-header-buttons.blade.php
+++ resources/views/backend/access/includes/partials/user-header-buttons.blade.php
@@ -9,15 +9,15 @@
     <span class="sr-only">Zwiń/Rozwiń</span>
   </button>
   <ul class="dropdown-menu" role="menu">
-    <li class="dropdown-item"><a href="{{route('admin.access.user.index')}}"><i class="fas fa-list-ul"></i>Wszyscy Użytkownicy</a></li>
+    <a href="{{route('admin.access.user.index')}}" class="dropdown-item"><i class="fas fa-list-ul"></i>Wszyscy Użytkownicy</a>
     @permission('create-user')
-    <li class="dropdown-item"><a href="{{route('admin.access.user.create')}}"><i class="fas fa-plus"></i>Dodaj Użytkownika</a></li>
+    <a href="{{route('admin.access.user.create')}}" class="dropdown-item"><i class="fas fa-plus"></i>Dodaj Użytkownika</a>
     @endauth
     @permission('view-deactive-user')
-    <li class="dropdown-item"><a href="{{route('admin.access.user.deactivated')}}"><i class="fas fa-square"></i>Nieaktywni Użytkownicy</a></li>
+    <a href="{{route('admin.access.user.deactivated')}}" class="dropdown-item"><i class="fas fa-square"></i>Nieaktywni Użytkownicy</a>
     @endauth
     @permission('view-deleted-user')
-    <li class="dropdown-item"><a href="{{route('admin.access.user.deleted')}}"><i class="fas fa-trash"></i>Usunięci Użytkownicy</a></li>
+    <a href="{{route('admin.access.user.deleted')}}" class="dropdown-item"><i class="fas fa-trash"></i>Usunięci Użytkownicy</a>
     @endauth
   </ul>
 </div>
