7d6e720c6d08f82e33d53267106c01f4290678b3
Kosu <sebastian@studiograficzne.com>
2022-03-17 11:05:28 +0100

    commit

--- resources/views/backend/access/users/index.blade.php
+++ resources/views/backend/access/users/index.blade.php
@@ -3,71 +3,75 @@
 @section('content')
 <div class="content">
     <div class="container-fluid">
-    <div class="card card-info">
-        <div class="card-header with-border">
-            <h3 class="card-title">Zarządzanie Użytkownikami</h3>
+        <div class="row">
+            <div class="col-md-12"> 
+                <div class="card">
+                    <div class="card-header card-header-primary">
+                        <h4 class="card-title">Zarządzanie Użytkownikami</h4>
+                        <p class="card-category"> Here is a subtitle for this table</p>
+                        <div class="card-tools float-right action-tools">
+                            @include('backend.access.includes.partials.user-header-buttons')
+                        </div>
+                    </div><!-- /.card-header --> 
 
-            <div class="card-tools float-right action-tools">
-                @include('backend.access.includes.partials.user-header-buttons')
+                    <div class="card-body">
+                        <div class="table-responsive data-table-wrapper">
+                            <table id="users-table" class="table table-condensed table-hover table-bordered">
+                                <thead class=" text-primary">
+                                    <tr>
+                                        <th>Imię</th>
+                                        <th>Nazwisko</th>
+                                        <th>E-mail</th>
+                                        <th>Potwierdzony ?</th>
+                                        <th>Role</th>
+                                        <th>Utworzony</th>
+                                        <th>Zmieniony</th>
+                                        <th>Akcje</th>
+                                    </tr>
+                                </thead>
+                                <thead class="transparent-bg">
+                                    <tr>
+                                        <th>
+                                            {!! Form::text('first_name', null, ["class" => "search-input-text form-control", "data-column" => 0, "placeholder" => 'Imię']) !!}
+                                            <a class="reset-data" href="javascript:void(0)"><i class="fas fa-times"></i></a>
+                                        </th>
+                                        <th>
+                                            {!! Form::text('last_name', null, ["class" => "search-input-text form-control", "data-column" => 1, "placeholder" => 'Nazwisko']) !!}
+                                            <a class="reset-data" href="javascript:void(0)"><i class="fas fa-times"></i></a>
+                                        </th>
+                                        <th>
+                                            {!! Form::text('email', null, ["class" => "search-input-text form-control", "data-column" => 2, "placeholder" => 'email']) !!}
+                                            <a class="reset-data" href="javascript:void(0)"><i class="fas fa-times"></i></a>
+                                        </th>
+                                        <th></th>
+                                        <th>
+                                            {!! Form::text('roles', null, ["class" => "search-input-text form-control", "data-column" => 4, "placeholder" => 'Role']) !!}
+                                            <a class="reset-data" href="javascript:void(0)"><i class="fas fa-times"></i></a>
+                                        </th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div><!--table-responsive-->
+                    </div><!-- /.card-body -->
+                </div><!--card-->
+
+                <div class="card card-info">
+                    <div class="card-header with-border">
+                        <h3 class="card-title">Historia</h3>
+                        <div class="card-tools pull-right">
+                            <button class="btn btn-card-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                        </div><!-- /.card tools -->
+                    </div><!-- /.card-header --> -->
+                    <div class="card-body">
+                        {{-- {!! history()->renderType('User') !!} --}}
+                    </div><!-- /.card-body -->
+                </div><!--card box-info-->
             </div>
-        </div><!-- /.card-header --> 
-
-        <div class="card-body">
-            <div class="table-responsive data-table-wrapper">
-                <table id="users-table" class="table table-condensed table-hover table-bordered">
-                    <thead>
-                        <tr>
-                            <th>Imię</th>
-                            <th>Nazwisko</th>
-                            <th>E-mail</th>
-                            <th>Potwierdzony ?</th>
-                            <th>Role</th>
-                            <th>Utworzony</th>
-                            <th>Zmieniony</th>
-                            <th>Akcje</th>
-                        </tr>
-                    </thead>
-                    <thead class="transparent-bg">
-                        <tr>
-                            <th>
-                                {!! Form::text('first_name', null, ["class" => "search-input-text form-control", "data-column" => 0, "placeholder" => 'Imię']) !!}
-                                    <a class="reset-data" href="javascript:void(0)"><i class="fas fa-times"></i></a>
-                                </th>
-                            <th>
-                                {!! Form::text('last_name', null, ["class" => "search-input-text form-control", "data-column" => 1, "placeholder" => 'Nazwisko']) !!}
-                                    <a class="reset-data" href="javascript:void(0)"><i class="fas fa-times"></i></a>
-                            </th>
-                            <th>
-                                {!! Form::text('email', null, ["class" => "search-input-text form-control", "data-column" => 2, "placeholder" => 'email']) !!}
-                                    <a class="reset-data" href="javascript:void(0)"><i class="fas fa-times"></i></a>
-                            </th>
-                            <th></th>
-                            <th>
-                            {!! Form::text('roles', null, ["class" => "search-input-text form-control", "data-column" => 4, "placeholder" => 'Role']) !!}
-                                <a class="reset-data" href="javascript:void(0)"><i class="fas fa-times"></i></a>
-                            </th>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                </table>
-            </div><!--table-responsive-->
-        </div><!-- /.card-body -->
-    </div><!--card-->
-
-    <div class="card card-info">
-        <div class="card-header with-border">
-            <h3 class="card-title">Historia</h3>
-            <div class="card-tools pull-right">
-                <button class="btn btn-card-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-            </div><!-- /.card tools -->
-        </div><!-- /.card-header --> -->
-        <div class="card-body">
-            {{-- {!! history()->renderType('User') !!} --}}
-        </div><!-- /.card-body -->
-    </div><!--card box-info-->
-</div>
+        </div>
+    </div>
 </div>
 @endsection
 
