8977f9d356707d0a95cecd057d038c7ddf52ba45
Kosu <sebastian@studiograficzne.com>
2022-03-17 13:14:55 +0100

    commit

--- resources/views/backend/access/includes/partials/user-header-buttons.blade.php
+++ resources/views/backend/access/includes/partials/user-header-buttons.blade.php
@@ -4,20 +4,20 @@
     @endif
 <!--Action Button-->
 <div class="btn-group">
-  <button type="button" class="btn btn-primary btn-flat dropdown-toggle" data-toggle="dropdown">@lang('menus.backend.access.users.action')
+  <button type="button" class="btn btn-primary btn-flat dropdown-toggle" data-toggle="dropdown">Działania
     <span class="caret"></span>
-    <span class="sr-only">Toggle Dropdown</span>
+    <span class="sr-only">Zwiń/Rozwiń</span>
   </button>
   <ul class="dropdown-menu" role="menu">
-    <li class="dropdown-item"><a href="{{route('admin.access.user.index')}}"><i class="fas fa-list-ul"></i> @lang('menus.backend.access.users.list')</a></li>
+    <li class="dropdown-item"><a href="{{route('admin.access.user.index')}}"><i class="fas fa-list-ul"></i>Wszyscy Użytkownicy</a></li>
     @permission('create-user')
-    <li class="dropdown-item"><a href="{{route('admin.access.user.create')}}"><i class="fas fa-plus"></i> @lang('menus.backend.access.users.add-new')</a></li>
+    <li class="dropdown-item"><a href="{{route('admin.access.user.create')}}"><i class="fas fa-plus"></i>Dodaj Użytkownika</a></li>
     @endauth
     @permission('view-deactive-user')
-    <li class="dropdown-item"><a href="{{route('admin.access.user.deactivated')}}"><i class="fas fa-square"></i> @lang('menus.backend.access.users.deactivated-users')</a></li>
+    <li class="dropdown-item"><a href="{{route('admin.access.user.deactivated')}}"><i class="fas fa-square"></i>Nieaktywni Użytkownicy</a></li>
     @endauth
     @permission('view-deleted-user')
-    <li class="dropdown-item"><a href="{{route('admin.access.user.deleted')}}"><i class="fas fa-trash"></i> @lang('menus.backend.access.users.deleted-users')</a></li>
+    <li class="dropdown-item"><a href="{{route('admin.access.user.deleted')}}"><i class="fas fa-trash"></i>Usunięci Użytkownicy</a></li>
     @endauth
   </ul>
 </div>
