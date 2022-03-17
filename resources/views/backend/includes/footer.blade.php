42c085dedf69098a467e10806fdc96ac86fe1ef9
Kosu <sebastian@studiograficzne.com>
2022-03-17 11:11:32 +0100

    commit

--- resources/views/backend/includes/footer.blade.php
+++ resources/views/backend/includes/footer.blade.php
@@ -11,11 +11,8 @@
 			</ul>
 		</nav>
 		<div class="copyright float-right">
-			&copy;
-			<script>
-				document.write(new Date().getFullYear())
-			</script>, wykonane z <i class="material-icons">favorite</i> przez
-			<a href="https://www.creative-tim.com" target="_blank">RAD Sp. z o.o.</a>
+			&copy;2022, wykonane z <i class="material-icons">favorite</i> przez
+			<a href="https://radspzoo.pl" target="_blank">RAD Sp. z o.o.</a>
 		</div>
 	</div>
 </footer>
