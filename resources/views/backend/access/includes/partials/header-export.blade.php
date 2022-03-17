a240c7aa7921ad0bd9658882e1abd28c7e0afce1
Kosu <sebastian@studiograficzne.com>
2022-03-17 13:19:32 +0100

    commit

--- resources/views/backend/access/includes/partials/header-export.blade.php
+++ resources/views/backend/access/includes/partials/header-export.blade.php
@@ -1,14 +1,14 @@
 <!--Action Button-->
 <div class="btn-group">
-  <button type="button" class="btn btn-warning btn-flat dropdown-toggle" data-toggle="dropdown">@lang('menus.backend.access.export')
+  <button type="button" class="btn btn-success btn-flat dropdown-toggle" data-toggle="dropdown">Eksport
     <span class="caret"></span>
-    <span class="sr-only">Toggle Dropdown</span>
+    <span class="sr-only">Zwiń/Rozwiń</span>
   </button>
   <ul class="dropdown-menu" role="menu">
-    <li id="copyButton" class="dropdown-item"><a href="#"><i class="fas fa-copy"></i> @lang('menus.backend.access.copy')</a></li>
+    <li id="copyButton" class="dropdown-item"><a href="#"><i class="fas fa-copy"></i> Kopiuj</a></li>
     <li id="csvButton" class="dropdown-item"><a href="#"><i class="far fa-file-text"></i> CSV</a></li>
     <li id="excelButton" class="dropdown-item"><a href="#"><i class="far fa-file-excel"></i> Excel</a></li>
     <li id="pdfButton" class="dropdown-item"><a href="#"><i class="far fa-file-pdf"></i> PDF</a></li>
-    <li id="printButton" class="dropdown-item"><a href="#"><i class="fas fa-print"></i> @lang('menus.backend.access.print')</a></li>
+    <li id="printButton" class="dropdown-item"><a href="#"><i class="fas fa-print"></i> Drukuj</a></li>
   </ul>
 </div>
