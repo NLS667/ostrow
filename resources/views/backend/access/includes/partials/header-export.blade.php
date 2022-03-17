518d1b88d94fff9dc17c7f1b02577c35b2528a9a
Kosu <sebastian@studiograficzne.com>
2022-03-17 14:30:32 +0100

    commit

--- resources/views/backend/access/includes/partials/header-export.blade.php
+++ resources/views/backend/access/includes/partials/header-export.blade.php
@@ -5,7 +5,7 @@
     <span class="sr-only">Zwiń/Rozwiń</span>
   </button>
   <ul class="dropdown-menu" role="menu">
-    <a href="#" class="dropdown-item"><i class="fa fa-clone"></i> Kopiuj</a>
+    <a href="#" class="dropdown-item"><span class="material-icons">content_copy</span> Kopiuj</a>
     <a href="#" class="dropdown-item"><i class="fa fa-file-text-o"></i> CSV</a>
     <a href="#" class="dropdown-item"><i class="fa fa-file-excel-o"></i> Excel</a>
     <a href="#" class="dropdown-item"><i class="fa fa-file-pdf-o"></i> PDF</a>
