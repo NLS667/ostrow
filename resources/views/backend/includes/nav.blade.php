484142a06ac246261f89a059da29906c4fdfdd50
Kosu <sebastian@studiograficzne.com>
2022-03-18 16:22:24 +0100

    commit

--- resources/views/backend/includes/nav.blade.php
+++ resources/views/backend/includes/nav.blade.php
@@ -3,8 +3,14 @@
   <div class="container-fluid">
     <!-- Left navbar links -->
     <div class="navbar-wrapper">
-      <a class="navbar-brand" href="#">{{ $titlePage }}</a>
-    </div>
+      <div class="navbar-minimize">
+        <button id="minimizeSidebar" class="btn btn-just-icon btn-white btn-fab btn-round">
+          <i class="material-icons text_align-center visible-on-sidebar-regular">more_vert</i>
+          <i class="material-icons design_bullet-list-67 visible-on-sidebar-mini">view_list</i>
+          <div class="ripple-container"></div></button>
+        </div>
+        <a class="navbar-brand" href="#">{{ $titlePage }}</a>
+      </div>
     <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
       <span class="sr-only">Toggle navigation</span>
       <span class="navbar-toggler-icon icon-bar"></span>
