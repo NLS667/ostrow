0f2a82ca3a5e2d1563039e905ff1230d630c0948
Kosu <sebastian@studiograficzne.com>
2022-03-18 09:59:26 +0100

    commit

--- resources/views/backend/access/roles/create.blade.php
+++ resources/views/backend/access/roles/create.blade.php
@@ -1,89 +1,87 @@
-@extends ('backend.layouts.app')
-
-@section ('title', trans('labels.backend.access.roles.management') . ' | ' . trans('labels.backend.access.roles.create'))
-
-@section('page-header')
-    <h1>
-        {{ trans('labels.backend.access.roles.management') }}
-        <small>{{ trans('labels.backend.access.roles.create') }}</small>
-    </h1>
-@endsection
+@extends ('backend.layouts.app', ['activePage' => 'role-management', 'titlePage' => 'Zarządzanie Rolami'])
 
 @section('content')
-    {{ Form::open(['route' => 'admin.access.role.store', 'class' => 'form-horizontal', 'role' => 'form', 'method' => 'post', 'id' => 'create-role']) }}
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                {{ Form::open(['route' => 'admin.access.role.store', 'class' => 'form-horizontal', 'role' => 'form', 'method' => 'post', 'id' => 'create-role']) }}
 
-        <div class="box box-info">
-            <div class="box-header with-border">
-                <h3 class="box-title">{{ trans('labels.backend.access.roles.create') }}</h3>
+                <div class="card">
+                    <div class="card-header card-header-info d-flex justify-content-between align-items-center">
+                        <h4 class="card-title">Utwórz Rolę</h4>
 
-                <div class="box-tools float-right">
-                    @include('backend.access.includes.partials.role-header-buttons')
-                </div><!--box-tools pull-right-->
-            </div><!-- /.box-header -->
+                        <div class="card-tools">
+                            @include('backend.access.includes.partials.role-header-buttons')
+                        </div><!--box-tools pull-right-->
+                    </div><!-- /.box-header -->
 
-            <div class="box-body">
-                <div class="form-group">
-                    {{ Form::label('name', trans('validation.attributes.backend.access.roles.name'), ['class' => 'col-lg-2 control-label required']) }}
+                    <div class="card-body">
+                        <div class="form-group">
+                            {{ Form::label('name', 'Nazwa', ['class' => 'col-lg-2 control-label required']) }}
 
-                    <div class="col-lg-10">
-                        {{ Form::text('name', null, ['class' => 'form-control box-size', 'placeholder' => trans('validation.attributes.backend.access.roles.name'), 'required' => 'required']) }}
-                    </div><!--col-lg-10-->
-                </div><!--form control-->
+                            <div class="col-lg-10">
+                                {{ Form::text('name', null, ['class' => 'form-control box-size', 'placeholder' => 'Nazwa', 'required' => 'required']) }}
+                            </div><!--col-lg-10-->
+                        </div><!--form control-->
 
-                <div class="form-group">
-                    {{ Form::label('associated_permissions', trans('validation.attributes.backend.access.roles.associated_permissions'), ['class' => 'col-lg-2 control-label']) }}
+                        <div class="form-group">
+                            {{ Form::label('associated_permissions', 'Przydzielone Uprawnienia', ['class' => 'col-lg-2 control-label']) }}
 
-                    <div class="col-lg-10">
-                        {{ Form::select('associated_permissions', array('all' => trans('labels.general.all'), 'custom' => trans('labels.general.custom')), 'all', ['class' => 'form-control select2 box-size']) }}
+                            <div class="col-lg-10">
+                                {{ Form::select('associated_permissions', array('all' => trans('labels.general.all'), 'custom' => trans('labels.general.custom')), 'all', ['class' => 'form-control select2 box-size']) }}
 
-                        <div id="available-permissions" class="hidden mt-20" style="width: 700px; height: 200px; overflow-x: hidden; overflow-y: scroll;">
-                            <div class="row">
-                                <div class="col-xs-12">
-                                    @if ($permissions->count())
-                                        @foreach ($permissions as $perm)
+                                <div id="available-permissions" class="hidden mt-20" style="width: 700px; height: 200px; overflow-x: hidden; overflow-y: scroll;">
+                                    <div class="row">
+                                        <div class="col-xs-12">
+                                            @if ($permissions->count())
+                                            @foreach ($permissions as $perm)
                                             <label class="control control--checkbox">
-                                            <input type="checkbox" name="permissions[{{ $perm->id }}]" value="{{ $perm->id }}" id="perm_{{ $perm->id }}" {{ is_array(old('permissions')) && in_array($perm->id, old('permissions')) ? 'checked' : '' }} /> <label for="perm_{{ $perm->id }}">{{ $perm->display_name }}</label>
-                                            <div class="control__indicator"></div>
+                                                <input type="checkbox" name="permissions[{{ $perm->id }}]" value="{{ $perm->id }}" id="perm_{{ $perm->id }}" {{ is_array(old('permissions')) && in_array($perm->id, old('permissions')) ? 'checked' : '' }} /> <label for="perm_{{ $perm->id }}">{{ $perm->display_name }}</label>
+                                                <div class="control__indicator"></div>
                                             </label>
                                             <br/>
-                                        @endforeach
-                                    @else
-                                        <p>There are no available permissions.</p>
-                                    @endif
-                                </div><!--col-lg-6-->
-                            </div><!--row-->
-                        </div><!--available permissions-->
-                    </div><!--col-lg-3-->
-                </div><!--form control-->
+                                            @endforeach
+                                            @else
+                                            <p>There are no available permissions.</p>
+                                            @endif
+                                        </div><!--col-lg-6-->
+                                    </div><!--row-->
+                                </div><!--available permissions-->
+                            </div><!--col-lg-3-->
+                        </div><!--form control-->
 
-                <div class="form-group">
-                    {{ Form::label('sort', trans('validation.attributes.backend.access.roles.sort'), ['class' => 'col-lg-2 control-label']) }}
+                        <div class="form-group">
+                            {{ Form::label('sort', trans('validation.attributes.backend.access.roles.sort'), ['class' => 'col-lg-2 control-label']) }}
 
-                    <div class="col-lg-10">
-                        {{ Form::text('sort', ($roleCount+1), ['class' => 'form-control box-size', 'placeholder' => trans('validation.attributes.backend.access.roles.sort')]) }}
-                    </div><!--col-lg-10-->
-                </div><!--form control-->
+                            <div class="col-lg-10">
+                                {{ Form::text('sort', ($roleCount+1), ['class' => 'form-control box-size', 'placeholder' => trans('validation.attributes.backend.access.roles.sort')]) }}
+                            </div><!--col-lg-10-->
+                        </div><!--form control-->
 
-                <div class="form-group">
-                    {{ Form::label('status', trans('validation.attributes.backend.access.roles.active'), ['class' => 'col-lg-2 control-label']) }}
+                        <div class="form-group">
+                            {{ Form::label('status', trans('validation.attributes.backend.access.roles.active'), ['class' => 'col-lg-2 control-label']) }}
 
-                    <div class="col-lg-10">
-                        <div class="control-group">
-                            <label class="control control--checkbox">
-                                {{ Form::checkbox('status', 1, true) }}
-                                <div class="control__indicator"></div>
-                            </label>
+                            <div class="col-lg-10">
+                                <div class="control-group">
+                                    <label class="control control--checkbox">
+                                        {{ Form::checkbox('status', 1, true) }}
+                                        <div class="control__indicator"></div>
+                                    </label>
+                                </div>
+                            </div><!--col-lg-3-->
+                        </div><!--form control-->
+                        <div class="edit-form-btn">
+                            {{ link_to_route('admin.access.role.index', 'Anuluj', [], ['class' => 'btn btn-danger btn-md']) }}
+                            {{ Form::submit('Stwórz', ['class' => 'btn btn-primary btn-md']) }}
                         </div>
-                    </div><!--col-lg-3-->
-                </div><!--form control-->
-                <div class="edit-form-btn">
-                    {{ link_to_route('admin.access.role.index', trans('buttons.general.cancel'), [], ['class' => 'btn btn-danger btn-md']) }}
-                    {{ Form::submit(trans('buttons.general.crud.create'), ['class' => 'btn btn-primary btn-md']) }}
-                    <div class="clearfix"></div>
-                </div>
-            </div><!-- /.box-body -->
-        </div><!--box-->
-    {{ Form::close() }}
+                    </div><!-- /.box-body -->
+                </div><!--box-->
+                {{ Form::close() }}
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
 
 @section('after-scripts')
