23e1a2c45cd209974e930db74fbe5bdef4bd85dd
Kosu <sebastian@studiograficzne.com>
2022-03-18 08:54:20 +0100

    commit

--- resources/views/backend/access/users/create.blade.php
+++ resources/views/backend/access/users/create.blade.php
@@ -17,16 +17,27 @@
               <div class="card-body ">
                 <div class="row">
                   <div class="col-md-12 text-right">
-                      <a href="{{ route('admin.access.user.index') }}" class="btn btn-sm btn-primary">{{ __('Back to list') }}</a>
+                      <a href="{{ route('admin.access.user.index') }}" class="btn btn-sm btn-primary">Powrót do listy</a>
                   </div>
                 </div>
                 <div class="row">
                   <label class="col-sm-2 col-form-label">Imię</label>
                   <div class="col-sm-7">
-                    <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
-                      <input class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" id="input-name" type="text" placeholder="Imię" value="{{ old('name') }}" required="true" aria-required="true"/>
-                      @if ($errors->has('name'))
-                        <span id="name-error" class="error text-danger" for="input-name">{{ $errors->first('name') }}</span>
+                    <div class="form-group{{ $errors->has('first_name') ? ' has-danger' : '' }}">
+                      <input class="form-control{{ $errors->has('first_name') ? ' is-invalid' : '' }}" name="name" id="input-first_name" type="text" placeholder="Imię" value="{{ old('first_name') }}" required="true" aria-required="true"/>
+                      @if ($errors->has('first_name'))
+                        <span id="name-error" class="error text-danger" for="input-first_name">{{ $errors->first('first_name') }}</span>
+                      @endif
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <label class="col-sm-2 col-form-label">Nazwisko</label>
+                  <div class="col-sm-7">
+                    <div class="form-group{{ $errors->has('last_name') ? ' has-danger' : '' }}">
+                      <input class="form-control{{ $errors->has('last_name') ? ' is-invalid' : '' }}" name="name" id="input-last_name" type="text" placeholder="Imię" value="{{ old('last_name') }}" required="true" aria-required="true"/>
+                      @if ($errors->has('last_name'))
+                        <span id="name-error" class="error text-danger" for="input-last_name">{{ $errors->first('last_name') }}</span>
                       @endif
                     </div>
                   </div>
@@ -57,7 +68,7 @@
                   <label class="col-sm-2 col-form-label" for="input-password-confirmation">Potwierdź Hasło</label>
                   <div class="col-sm-7">
                     <div class="form-group">
-                      <input class="form-control" name="password_confirmation" id="input-password-confirmation" type="password" placeholder="{{ __('Confirm Password') }}" value="" required />
+                      <input class="form-control" name="password_confirmation" id="input-password-confirmation" type="password" placeholder="Potwierdź Hasło" value="" required />
                     </div>
                   </div>
                 </div>
