c1aafccb5efef2df1fa0e3ac5132bf55de4e43c2
Kosu <sebastian@studiograficzne.com>
2022-03-18 16:39:01 +0100

    commit

--- resources/views/backend/access/includes/partials/header-export.blade.php
+++ resources/views/backend/access/includes/partials/header-export.blade.php
@@ -5,10 +5,10 @@
     <span class="sr-only">Zwiń/Rozwiń</span>
   </button>
   <ul class="dropdown-menu" role="menu">
-    <a href="#" class="dropdown-item"><i class="fa-solid fa-copy"></i> Kopiuj</a>
-    <a href="#" class="dropdown-item"><i class="fa-solid fa-file-csv"></i> CSV</a>
-    <a href="#" class="dropdown-item"><i class="fa-solid fa-file-excel"></i> Excel</a>
-    <a href="#" class="dropdown-item"><i class="fa-solid fa-file-pdf"></i> PDF</a>
-    <a href="#" class="dropdown-item"><i class="fa-solid fa-print"></i> Drukuj</a>
+    <a href="#" class="dropdown-item" id="copyButton"><i class="fa-solid fa-copy"></i> Kopiuj</a>
+    <a href="#" class="dropdown-item" id="csvButton"><i class="fa-solid fa-file-csv"></i> CSV</a>
+    <a href="#" class="dropdown-item" id="excelButton"><i class="fa-solid fa-file-excel"></i> Excel</a>
+    <a href="#" class="dropdown-item" id="pdfButton"><i class="fa-solid fa-file-pdf"></i> PDF</a>
+    <a href="#" class="dropdown-item" id="printButton"><i class="fa-solid fa-print"></i> Drukuj</a>
   </ul>
 </div>
