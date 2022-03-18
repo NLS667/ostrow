346551dd89806ddba4782e3a6c2c17c23e848929
Kosu <sebastian@studiograficzne.com>
2022-03-18 10:23:04 +0100

    commit

--- resources/views/backend/access/permissions/create.blade.php
+++ resources/views/backend/access/permissions/create.blade.php
@@ -1,37 +1,34 @@
-@extends ('backend.layouts.app')
-
-@section ('title', trans('labels.backend.access.permissions.management') . ' | ' . trans('labels.backend.access.permissions.create'))
-
-@section('page-header')
-    <h1>
-        {{ trans('labels.backend.access.permissions.management') }}
-        <small>{{ trans('labels.backend.access.permissions.create') }}</small>
-    </h1>
-@endsection
+@extends ('backend.layouts.app', ['activePage' => 'permission-management', 'titlePage' => 'Zarządzanie Uprawnieniami'])
 
 @section('content')
-    {{ Form::open(['route' => 'admin.access.permission.store', 'class' => 'form-horizontal', 'role' => 'form', 'method' => 'post', 'id' => 'create-permission']) }}
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                {{ Form::open(['route' => 'admin.access.permission.store', 'class' => 'form-horizontal', 'role' => 'form', 'method' => 'post', 'id' => 'create-permission']) }}
 
-        <div class="box box-info">
-            <div class="box-header with-border">
-                <h3 class="box-title">{{ trans('labels.backend.access.permissions.create') }}</h3>
+                <div class="card">
+                    <div class="card-header card-header-info d-flex justify-content-between align-items-center">
+                        <h4 class="box-title">Utwórz Uprawnienie</h4>
+                        <div class="card-toolst">
+                            @include('backend.access.includes.partials.permission-header-buttons')
+                        </div><!--box-tools pull-right-->
+                    </div><!-- /.box-header -->
 
-                <div class="box-tools float-right">
-                    @include('backend.access.includes.partials.permission-header-buttons')
-                </div><!--box-tools pull-right-->
-            </div><!-- /.box-header -->
+                    <div class="card-body">
 
-            <div class="box-body">
+                        {{-- Including Form --}}
+                        @include("backend.access.permissions.form")
 
-                {{-- Including Form --}}
-                @include("backend.access.permissions.form")
-
-                <div class="edit-form-btn">
-                    {{ link_to_route('admin.access.permission.index', trans('buttons.general.cancel'), [], ['class' => 'btn btn-danger btn-md']) }}
-                    {{ Form::submit(trans('buttons.general.crud.create'), ['class' => 'btn btn-primary btn-md']) }}
-                    <div class="clearfix"></div>
-                </div>
-            </div><!-- /.box-body -->
-        </div><!--box-->
-    {{ Form::close() }}
+                        <div class="edit-form-btn">
+                            {{ link_to_route('admin.access.permission.index', 'Anuluj', [], ['class' => 'btn btn-danger btn-md']) }}
+                            {{ Form::submit('Dodaj', ['class' => 'btn btn-primary btn-md']) }}
+                        </div>
+                    </div><!-- /.box-body -->
+                </div><!--box-->
+                {{ Form::close() }}
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
