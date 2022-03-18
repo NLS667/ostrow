a61070133c223fb0d7127038eb1aced43740103c
Kosu <sebastian@studiograficzne.com>
2022-03-18 09:50:21 +0100

    commit

--- resources/views/backend/access/roles/edit.blade.php
+++ resources/views/backend/access/roles/edit.blade.php
@@ -1,67 +1,75 @@
 @extends ('backend.layouts.app', ['activePage' => 'role-management', 'titlePage' => 'Zarządzanie Rolami'])
 
 @section('content')
-    {{ Form::model($role, ['route' => ['admin.access.role.update', $role], 'class' => 'form-horizontal', 'role' => 'form', 'method' => 'PATCH', 'id' => 'edit-role']) }}
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                {{ Form::model($role, ['route' => ['admin.access.role.update', $role], 'class' => 'form-horizontal', 'role' => 'form', 'method' => 'PATCH', 'id' => 'edit-role']) }}
 
-        <div class="card">
-            <div class="card-header card-header-info d-flex justify-content-between align-items-center">
-                <h4 class="card-title">Zarządzanie Rolami</h4>
+                <div class="card">
+                    <div class="card-header card-header-info d-flex justify-content-between align-items-center">
+                        <h4 class="card-title">Zarządzanie Rolami</h4>
 
-                <div class="card-tools">
-                    @include('backend.access.includes.partials.role-header-buttons')
-                </div><!--box-tools pull-right-->
-            </div><!-- /.box-header -->
+                        <div class="card-tools">
+                            @include('backend.access.includes.partials.role-header-buttons')
+                        </div><!--box-tools pull-right-->
+                    </div><!-- /.box-header -->
 
-            <div class="card-body">
-                <div class="form-group">
-                    {{ Form::label('name', 'Nazwa', ['class' => 'col-lg-2 control-label required']) }}
+                    <div class="card-body">
+                        <div class="form-group">
+                            {{ Form::label('name', 'Nazwa', ['class' => 'col-lg-2 control-label required']) }}
 
-                    <div class="col-lg-10">
-                        {{ Form::text('name', null, ['class' => 'form-control box-size', 'placeholder' => 'Nazwa', 'required' => 'required']) }}
-                    </div><!--col-lg-10-->
-                </div><!--form control-->
+                            <div class="col-lg-10">
+                                {{ Form::text('name', null, ['class' => 'form-control box-size', 'placeholder' => 'Nazwa', 'required' => 'required']) }}
+                            </div><!--col-lg-10-->
+                        </div><!--form control-->
 
-                <div class="form-group">
-                    {{ Form::label('associated_permissions', 'Przypisane Uprawnienia', ['class' => 'col-lg-2 control-label']) }}
+                        <div class="form-group">
+                            {{ Form::label('associated_permissions', 'Przypisane Uprawnienia', ['class' => 'col-lg-2 control-label']) }}
 
-                    <div class="col-lg-10">
-                        {{ Form::select('associated_permissions', ['all' => 'Wszystkie', 'custom' => 'Wybrane'], $role->all ? 'wszystkie' : 'wybrane', ['class' => 'form-control select2 box-size']) }}
+                            <div class="col-lg-10">
+                                {{ Form::select('associated_permissions', ['all' => 'Wszystkie', 'custom' => 'Wybrane'], $role->all ? 'wszystkie' : 'wybrane', ['class' => 'form-control select2 box-size']) }}
 
-                        <div id="available-permissions" class="hidden mt-20" style="width: 700px; height: 200px; overflow-x: hidden; overflow-y: scroll;">
-                            <div class="row">
-                                <div class="col-xs-12">
-                                    @if ($permissions->count())
-                                        @foreach ($permissions as $perm)
-                                        <label class="control control--checkbox">
-                                            <input type="checkbox" name="permissions[{{ $perm->id }}]" value="{{ $perm->id }}" id="perm_{{ $perm->id }}" {{ is_array(old('permissions')) ? (in_array($perm->id, old('permissions')) ? 'checked' : '') : (in_array($perm->id, $rolePermissions) ? 'checked' : '') }} /> <label for="perm_{{ $perm->id }}">{{ $perm->display_name }}</label>
-                                            <div class="control__indicator"></div>
+                                <div id="available-permissions" class="hidden mt-20" style="width: 700px; height: 200px; overflow-x: hidden; overflow-y: scroll;">
+                                    <div class="row">
+                                        <div class="col-xs-12">
+                                            @if ($permissions->count())
+                                            @foreach ($permissions as $perm)
+                                            <label class="control control--checkbox">
+                                                <input type="checkbox" name="permissions[{{ $perm->id }}]" value="{{ $perm->id }}" id="perm_{{ $perm->id }}" {{ is_array(old('permissions')) ? (in_array($perm->id, old('permissions')) ? 'checked' : '') : (in_array($perm->id, $rolePermissions) ? 'checked' : '') }} /> <label for="perm_{{ $perm->id }}">{{ $perm->display_name }}</label>
+                                                <div class="control__indicator"></div>
                                             </label>
                                             <br/>
-                                        @endforeach
-                                    @else
-                                        <p>Brak dostępnych uprawnień.</p>
-                                    @endif
-                                </div><!--col-lg-6-->
-                            </div><!--row-->
-                        </div><!--available permissions-->
-                    </div><!--col-lg-3-->
-                </div><!--form control-->
+                                            @endforeach
+                                            @else
+                                            <p>Brak dostępnych uprawnień.</p>
+                                            @endif
+                                        </div><!--col-lg-6-->
+                                    </div><!--row-->
+                                </div><!--available permissions-->
+                            </div><!--col-lg-3-->
+                        </div><!--form control-->
 
-                <div class="form-group">
-                    {{ Form::label('sort', 'Kolejność', ['class' => 'col-lg-2 control-label']) }}
+                        <div class="form-group">
+                            {{ Form::label('sort', 'Kolejność', ['class' => 'col-lg-2 control-label']) }}
 
-                    <div class="col-lg-10">
-                        {{ Form::text('sort', null, ['class' => 'form-control box-size', 'placeholder' => 'Kolejność']) }}
-                    </div><!--col-lg-10-->
-                </div><!--form control-->
-                <div class="edit-form-btn">
-                    {{ link_to_route('admin.access.role.index', 'Anuluj', [], ['class' => 'btn btn-danger btn-md']) }}
-                    {{ Form::submit('Zmień', ['class' => 'btn btn-primary btn-md']) }}
-                    <div class="clearfix"></div>
-                </div>
-            </div><!-- /.box-body -->
-        </div><!--box-->
-    {{ Form::close() }}
+                            <div class="col-lg-10">
+                                {{ Form::text('sort', null, ['class' => 'form-control box-size', 'placeholder' => 'Kolejność']) }}
+                            </div><!--col-lg-10-->
+                        </div><!--form control-->
+                        <div class="edit-form-btn">
+                            {{ link_to_route('admin.access.role.index', 'Anuluj', [], ['class' => 'btn btn-danger btn-md']) }}
+                            {{ Form::submit('Zmień', ['class' => 'btn btn-primary btn-md']) }}
+                            <div class="clearfix"></div>
+                        </div>
+                    </div><!-- /.box-body -->
+                </div><!--box-->
+                {{ Form::close() }}
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
 
 @section('after-scripts')
