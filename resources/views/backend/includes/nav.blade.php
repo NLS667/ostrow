cbd1df05980f27e79b66489d5660f4954881df70
Kosu <sebastian@studiograficzne.com>
2022-03-18 16:12:07 +0100

    commit

--- resources/views/backend/includes/nav.blade.php
+++ resources/views/backend/includes/nav.blade.php
@@ -3,7 +3,7 @@
   <div class="container-fluid">
     <!-- Left navbar links -->
     <div class="navbar-wrapper">
-      <a class="navbar-brand" href="#">Strona główna</a>
+      <a class="navbar-brand" href="#">{{ $titlePage }}</a>
     </div>
     <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
       <span class="sr-only">Toggle navigation</span>
