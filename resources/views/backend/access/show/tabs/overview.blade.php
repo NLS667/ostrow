08cfc2385b33f67fb4006573b25345c7d0aff3bf
Kosu <sebastian@studiograficzne.com>
2022-03-18 09:25:28 +0100

    commit

--- resources/views/backend/access/show/tabs/overview.blade.php
+++ resources/views/backend/access/show/tabs/overview.blade.php
@@ -1,37 +1,32 @@
 <table class="table table-striped table-hover">
     <tr>
-        <th>{{ trans('labels.backend.access.users.tabs.content.overview.name') }}</th>
+        <th>Imię</th>
         <td>{{ $user->first_name .' '. $user->last_name }}</td>
     </tr>
 
     <tr>
-        <th>{{ trans('labels.backend.access.users.tabs.content.overview.email') }}</th>
+        <th>Nazwisko</th>
         <td>{{ $user->email }}</td>
     </tr>
 
     <tr>
-        <th>{{ trans('labels.backend.access.users.tabs.content.overview.status') }}</th>
-        <td>{!! $user->status_label !!}</td>
-    </tr>
-
-    <tr>
-        <th>{{ trans('labels.backend.access.users.tabs.content.overview.confirmed') }}</th>
+        <th>Potwierdzony?</th>
         <td>{!! $user->confirmed_label !!}</td>
     </tr>
 
     <tr>
-        <th>{{ trans('labels.backend.access.users.tabs.content.overview.created_at') }}</th>
+        <th>Utworzony</th>
         <td>{{ $user->created_at }} ({{ $user->created_at->diffForHumans() }})</td>
     </tr>
 
     <tr>
-        <th>{{ trans('labels.backend.access.users.tabs.content.overview.last_updated') }}</th>
+        <th>Ostatnia modyfikacja</th>
         <td>{{ $user->updated_at }} ({{ $user->updated_at->diffForHumans() }})</td>
     </tr>
 
     @if ($user->trashed())
         <tr>
-            <th>{{ trans('labels.backend.access.users.tabs.content.overview.deleted_at') }}</th>
+            <th>Usunięty</th>
             <td>{{ $user->deleted_at }} ({{ $user->deleted_at->diffForHumans() }})</td>
         </tr>
     @endif
