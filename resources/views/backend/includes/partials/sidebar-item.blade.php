01a29eb883d1c507bdd6b830124a97a4ca04d694
Kosu <sebastian@studiograficzne.com>
2022-03-21 18:34:47 +0100

    commit

--- resources/views/backend/includes/partials/sidebar-item.blade.php
+++ resources/views/backend/includes/partials/sidebar-item.blade.php
@@ -1,12 +1,12 @@
 <li class="{{ active_class(isActiveMenuItem($item), 'active') }} nav-item">
     @if (!empty($item->children))
         <a class="nav-link"data-toggle="collapse" aria-expanded="true" href="#{{ $item->content }}">
-            <i class="material-icons">{{ @$item->icon }}</i>
-            <p>{{ $item->name }}<b class="caret"></b></p>
+            <span class="sidebar-mini"><i class="material-icons">{{ @$item->icon }}</i></span>
+            <span class="sidebar-normal">{{ $item->name }}<b class="caret"></b></span>
     @else
         <a class="nav-link" href="{{ getRouteUrl($item->url, $item->url_type) }}">
-            <i class="material-icons">{{ @$item->icon }}</i>
-            <p>{{ $item->name }}</p>
+            <span class="sidebar-mini"><i class="material-icons">{{ @$item->icon }}</i></span>
+            <span class="sidebar-normal">{{ $item->name }}</span>
     @endif        
         </a>
     @if (!empty($item->children))
