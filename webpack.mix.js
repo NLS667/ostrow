0650d0fc5d6a4c74b789e20c00d970491a7d5c0e
Kosu <sebastian@studiograficzne.com>
2022-03-21 09:22:06 +0100

    commit

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
+/*
 mix.autoload({
         jquery: ['$', 'window.jQuery', 'jQuery'],
         'popper.js/dist/umd/popper.js': ['Popper']
@@ -32,6 +33,48 @@ mix.autoload({
         "resources/assets/js/plugins/settings.js", 
         //'resources/assets/js/plugins/bootstrap-material-design.min.js',
         'resources/assets/js/plugins/material-dashboard2.js',
+    ], 'public/js/app-custom.js')
+    .vue()
+    .version();
+*/
+
+mix.autoload({
+        jquery: ['$', 'window.jQuery', 'jQuery'],
+        'popper.js/dist/umd/popper.js': ['Popper']
+    })
+    .sass('resources/assets/sass/front.scss', 'public/css/front.css')
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .styles([
+        'resources/assets/sass/plugins/jquery.dataTables.min.css',
+        'node_modules/datatables.net-bs4/css/dataTables.bootstrap4.min.css',
+        'node_modules/select2/dist/css/select2.css',
+    ], 'public/css/app-custom.css')
+    .js([
+        'resources/assets/js/front.js',
+        'resources/assets/js/plugins/paper-kit.js',
+    ], 'public/js/front.js')
+    .js('resources/assets/js/app.js', 'public/js')
+    .scripts([
+        "resources/assets/js/plugins/common.js",
+        'resources/assets/js/plugins/bootstrap-material-design.min.js',
+        'resources/assets/js/plugins/perfect-scrollbar.jquery.min.js',
+        'resources/assets/js/plugins/moment.min.js',
+        'resources/assets/js/plugins/sweetalert2.js',
+        'resources/assets/js/plugins/jquery.validate.min.js',
+        'resources/assets/js/plugins/jquery.bootstrap-wizard.js',
+        'resources/assets/js/plugins/bootstrap-selectpicker.js',
+        'resources/assets/js/plugins/bootstrap-datetimepicker.min.js',
+        'resources/assets/js/plugins/jquery.dataTables.min.js',
+        'resources/assets/js/plugins/bootstrap-tagsinput.js',
+        'resources/assets/js/plugins/jasny-bootstrap.min.js',
+        'resources/assets/js/plugins/fullcalendar.min.js',
+        'resources/assets/js/plugins/jquery-jvectormap.js',
+        'resources/assets/js/plugins/nouislider.min.js',
+        'resources/assets/js/plugins/arrive.min.js',
+        'resources/assets/js/plugins/chartist.min.js',
+        'resources/assets/js/plugins/bootstrap-notify.js',
+        'resources/assets/js/plugins/material-dashboard.js',
+        'resources/assets/js/plugins/settings.js',
     ], 'public/js/app-custom.js')
     .vue()
     .version();
