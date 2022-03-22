af5ee03116cfd11606b352482cec66a80b695def
Kosu <sebastian@studiograficzne.com>
2022-03-22 14:11:15 +0100

    commit

--- resources/views/backend/access/roles/edit.blade.php
+++ resources/views/backend/access/roles/edit.blade.php
@@ -85,45 +85,6 @@
                                 </div>
                             </div>
                         </div>
-
-                        <div class="form-group">
-                            {{ Form::label('associated_permissions', 'Przypisane Uprawnienia', ['class' => 'col-lg-2 control-label']) }}
-
-                            <div class="col-lg-10">
-                                {{ Form::select('associated_permissions', ['all' => 'Wszystkie', 'custom' => 'Wybrane'], $role->all ? 'wszystkie' : 'wybrane', ['class' => 'form-control select2 box-size']) }}
-
-                                <div id="available-permissions" class="d-none container" style="margin-top:10px; height: 200px; overflow-x: hidden; overflow-y: scroll;">
-                                    <div class="row">
-                                        <div class="col-xs-12">
-                                            @if ($permissions->count())
-                                            @foreach ($permissions as $perm)
-                                            <label class="control control--checkbox">
-                                                <input type="checkbox" name="permissions[{{ $perm->id }}]" value="{{ $perm->id }}" id="perm_{{ $perm->id }}" {{ is_array(old('permissions')) ? (in_array($perm->id, old('permissions')) ? 'checked' : '') : (in_array($perm->id, $rolePermissions) ? 'checked' : '') }} /> <label for="perm_{{ $perm->id }}">{{ $perm->display_name }}</label>
-                                                <div class="control__indicator"></div>
-                                            </label>
-                                            <br/>
-                                            @endforeach
-                                            @else
-                                            <p>Brak dostępnych uprawnień.</p>
-                                            @endif
-                                        </div><!--col-lg-6-->
-                                    </div><!--row-->
-                                </div><!--available permissions-->
-                            </div><!--col-lg-3-->
-                        </div><!--form control-->
-
-                        <div class="form-group">
-                            {{ Form::label('sort', 'Kolejność', ['class' => 'col-lg-2 control-label']) }}
-
-                            <div class="col-lg-10">
-                                {{ Form::text('sort', null, ['class' => 'form-control box-size', 'placeholder' => 'Kolejność']) }}
-                            </div><!--col-lg-10-->
-                        </div><!--form control-->
-                        <div class="edit-form-btn">
-                            {{ link_to_route('admin.access.role.index', 'Anuluj', [], ['class' => 'btn btn-danger btn-md']) }}
-                            {{ Form::submit('Zmień', ['class' => 'btn btn-primary btn-md']) }}
-                            <div class="clearfix"></div>
-                        </div>
                     </div><!-- /.box-body -->
                 </div><!--box-->
                 {{ Form::close() }}
@@ -136,6 +97,21 @@
 @section('after-scripts')
     
     <script type="text/javascript">
+        var associated = $("select[name='associated_permissions']");
+        var associated_container = $("#available-permissions");
+
+        if (associated.val() == "custom")
+            associated_container.removeClass('d-none');
+        else
+            associated_container.addClass('d-none');
+
+        associated.change(function() {
+            if ($(this).val() == "custom")
+                associated_container.removeClass('d-none');
+            else
+                associated_container.addClass('d-none');
+        });
+        
         Backend.Utils.documentReady(function(){
              Backend.Roles.init("edit")
         });
