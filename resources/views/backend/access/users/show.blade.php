02dd2f464ce4c1388de7eb8da80e6568b25c275e
Kosu <sebastian@studiograficzne.com>
2022-03-22 09:46:18 +0100

    commit

--- resources/views/backend/access/users/show.blade.php
+++ resources/views/backend/access/users/show.blade.php
@@ -5,43 +5,42 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
-    <div class="card">
-        <div class="card-header card-header-info d-flex justify-content-between align-items-center">
-            <h4 class="card-title">Podgląd Użytkownika</h4>
+                <div class="card">
+                    <div class="card-header card-header-info d-flex justify-content-between align-items-center">
+                        <h4 class="card-title">Podgląd Użytkownika</h4>
 
-            <div class="card-tools">
-                @include('backend.access.includes.partials.user-header-buttons')
-            </div><!--box-tools pull-right-->
-        </div><!-- /.box-header -->
+                        <div class="card-tools">
+                            @include('backend.access.includes.partials.user-header-buttons')
+                        </div><!--box-tools pull-right-->
+                    </div><!-- /.box-header -->
 
-        <div role="tabpanel">
+                    <div role="card-body">
+                        <!-- Nav tabs -->
+                        <ul class="nav nav-pills nav-pills-warning" role="tablist">
+                            <li role="presentation" class="nav-item">
+                                <a class="nav-link active show" href="#overview" aria-controls="overview" role="tablist" data-toggle="tab">Informacje Ogólne</a>
+                            </li>
 
-                <!-- Nav tabs -->
-                <ul class="nav nav-pills nav-pills-warning" role="tablist">
-                    <li role="presentation" class="nav-item">
-                        <a class="nav-link active show" href="#overview" aria-controls="overview" role="tablist" data-toggle="tab">Informacje Ogólne</a>
-                    </li>
+                            <li role="presentation" class="nav-item">
+                                <a class="nav-link" href="#history" aria-controls="history" role="tablist" data-toggle="tab">Historia</a>
+                            </li>
+                        </ul>
 
-                    <li role="presentation" class="nav-item">
-                        <a class="nav-link" href="#history" aria-controls="history" role="tablist" data-toggle="tab">Historia</a>
-                    </li>
-                </ul>
+                        <div class="tab-content tab-space">
 
-                <div class="tab-content tab-space">
+                            <div role="tabpanel" class="tab-pane active show" id="overview">
+                                @include('backend.access.show.tabs.overview')
+                            </div><!--tab overview profile-->
 
-                    <div role="tabpanel" class="tab-pane active show" id="overview">
-                        @include('backend.access.show.tabs.overview')
-                    </div><!--tab overview profile-->
+                            <div role="tabpanel" class="tab-pane" id="history">
+                                @include('backend.access.show.tabs.history')
+                            </div><!--tab panel history-->
 
-                    <div role="tabpanel" class="tab-pane" id="history">
-                        @include('backend.access.show.tabs.history')
-                    </div><!--tab panel history-->
-
-                </div><!--tab content-->
-        </div><!-- /.box-body -->
-    </div><!--box-->
-</div>
-</div>
-</div>
+                        </div><!--tab content-->
+                    </div><!-- /.box-body -->
+                </div><!--box-->
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
