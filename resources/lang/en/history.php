e74a004cc4c6b98bf430f9c4c9d3e75241e41733
Kosu <sebastian@studiograficzne.com>
2022-03-22 10:01:58 +0100

    commit

---
+++ resources/lang/en/history.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | History Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain strings associated to the
+    | system adding lines to the history table.
+    |
+    */
+
+    'backend' => [
+        'none'            => 'Brak zdarzeń w historii.',
+        'none_for_type'   => 'Brak zdarzeń w historii tego typu.',
+        'none_for_entity' => 'Brak zdarzeń w historii dla :entity.',
+        'recent_history'  => 'Historia',
+
+        'roles' => [
+            'created' => 'utworzył rolę.',
+            'deleted' => 'usunął rolę.',
+            'updated' => 'zaktualizował rolę.',
+        ],
+        'permissions' => [
+            'created' => 'utworzył uprawnienie',
+            'deleted' => 'usunął uprawnienie',
+            'updated' => 'zaktualizował uprawnienie',
+        ],
+        'users' => [
+            'changed_password'    => 'zmienił hasło użytkownika',
+            'created'             => 'utworzył użytkownika',
+            'deactivated'         => 'deaktywował użytkownika',
+            'deleted'             => 'usunął użytkownika',
+            'permanently_deleted' => 'permanently usunął użytkownika',
+            'updated'             => 'zaktualizował użytkownika',
+            'reactivated'         => 'reaktywował użytkownika',
+            'restored'            => 'przywrócił użytkownika',
+        ],
+    ],
+];
