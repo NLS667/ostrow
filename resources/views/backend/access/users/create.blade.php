9ba3bbed654d2bd39fbfa554aba55fc25077c021
Kosu <sebastian@studiograficzne.com>
2022-03-23 11:32:35 +0100

    commit

--- resources/views/backend/access/users/create.blade.php
+++ resources/views/backend/access/users/create.blade.php
@@ -27,7 +27,7 @@
                 <label class="col-sm-2 col-form-label">Imię</label>
                 <div class="col-sm-7">
                   <div class="form-group{{ $errors->has('first_name') ? ' has-danger' : '' }}">
-                    <input class="form-control{{ $errors->has('first_name') ? ' is-invalid' : '' }}" name="name" id="input-first_name" type="text" placeholder="Imię" value="{{ old('first_name') }}" required="true" aria-required="true"/>
+                    <input class="form-control{{ $errors->has('first_name') ? ' is-invalid' : '' }}" name="first_name" id="input-first_name" type="text" placeholder="Imię" value="{{ old('first_name') }}" required="true" aria-required="true"/>
                     @if ($errors->has('first_name'))
                     <span id="name-error" class="error text-danger" for="input-first_name">{{ $errors->first('first_name') }}</span>
                     @endif
@@ -40,7 +40,7 @@
                 <label class="col-sm-2 col-form-label">Nazwisko</label>
                 <div class="col-sm-7">
                   <div class="form-group{{ $errors->has('last_name') ? ' has-danger' : '' }}">
-                    <input class="form-control{{ $errors->has('last_name') ? ' is-invalid' : '' }}" name="name" id="input-last_name" type="text" placeholder="Nazwisko" value="{{ old('last_name') }}" required="true" aria-required="true"/>
+                    <input class="form-control{{ $errors->has('last_name') ? ' is-invalid' : '' }}" name="last_name" id="input-last_name" type="text" placeholder="Nazwisko" value="{{ old('last_name') }}" required="true" aria-required="true"/>
                     @if ($errors->has('last_name'))
                     <span id="name-error" class="error text-danger" for="input-last_name">{{ $errors->first('last_name') }}</span>
                     @endif
