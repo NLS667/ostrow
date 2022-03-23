ceeed2d32b71c5c969f054ea4ca13437c49826ca
Kosu <sebastian@studiograficzne.com>
2022-03-23 15:11:27 +0100

    commit

--- resources/views/backend/access/users/edit.blade.php
+++ resources/views/backend/access/users/edit.blade.php
@@ -24,7 +24,7 @@
                   <label class="col-sm-2 col-form-label">Imię</label>
                   <div class="col-sm-7">
                     <div class="form-group{{ $errors->has('first_name') ? ' has-danger' : '' }}">
-                      <input class="form-control{{ $errors->has('first_name') ? ' is-invalid' : '' }}" name="name" id="input-first_name" type="text" placeholder="Imię" value="{{ old('first_name', $user->first_name) }}" required="true" aria-required="true"/>
+                      <input class="form-control{{ $errors->has('first_name') ? ' is-invalid' : '' }}" name="first_name" id="input-first_name" type="text" placeholder="Imię" value="{{ old('first_name', $user->first_name) }}" required="true" aria-required="true"/>
                       @if ($errors->has('first_name'))
                         <span id="name-error" class="error text-danger" for="input-first_name">{{ $errors->first('first_name') }}</span>
                       @endif
@@ -35,7 +35,7 @@
                   <label class="col-sm-2 col-form-label">Nazwisko</label>
                   <div class="col-sm-7">
                     <div class="form-group{{ $errors->has('last_name') ? ' has-danger' : '' }}">
-                      <input class="form-control{{ $errors->has('last_name') ? ' is-invalid' : '' }}" name="name" id="input-last_name" type="text" placeholder="Imię" value="{{ old('last_name', $user->last_name) }}" required="true" aria-required="true"/>
+                      <input class="form-control{{ $errors->has('last_name') ? ' is-invalid' : '' }}" name="last_name" id="input-last_name" type="text" placeholder="Imię" value="{{ old('last_name', $user->last_name) }}" required="true" aria-required="true"/>
                       @if ($errors->has('last_name'))
                         <span id="name-error" class="error text-danger" for="input-last_name">{{ $errors->first('last_name') }}</span>
                       @endif
