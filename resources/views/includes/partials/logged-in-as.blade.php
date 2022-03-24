1d4b85f020d728b6f9ff4af0cf3351e4b4c56da2
Kosu <sebastian@studiograficzne.com>
2022-03-24 11:25:08 +0100

    commit

--- resources/views/includes/partials/logged-in-as.blade.php
+++ resources/views/includes/partials/logged-in-as.blade.php
@@ -1,5 +1,9 @@
 @if ($logged_in_user && session()->has("admin_user_id") && session()->has("temp_user_id"))
-    <div class="alert alert-warning logged-in-as">
-        Jesteś aktualnie zalogowany jako {{ $logged_in_user->first_name }}. <a href="{{ route("auth.logout-as") }}">Re-Loguj jako {{ session()->get("admin_user_name") }}</a>.
-    </div><!--alert alert-warning logged-in-as-->
+<div id="message">
+    <div style="padding: 5px;">
+	    <div class="alert alert-warning logged-in-as">
+	        Jesteś aktualnie zalogowany jako {{ $logged_in_user->first_name }}. <a href="{{ route("auth.logout-as") }}">Re-Loguj jako {{ session()->get("admin_user_name") }}</a>.
+	    </div><!--alert alert-warning logged-in-as-->
+	</div>
+</div>
 @endif
