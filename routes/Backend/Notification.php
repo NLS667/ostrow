0a8a6168051bbc5eb4478db48017e3c98cd23080
Kosu <sebastian@studiograficzne.com>
2022-03-24 16:44:40 +0100

    commit

--- routes/Backend/Notification.php
+++ routes/Backend/Notification.php
@@ -1,16 +1,16 @@
 <?php
 
-use App\Http\Controllers\Backend\NotificationController;
+//use App\Http\Controllers\Backend\NotificationController;
 
 /*
  * Notificatons Management
  */
 Route::resource('notification', NotificationController::class, ['except' => ['show', 'create', 'store']]);
 
-Route::get('notification/getlist', [NotificationController::class, 'ajaxNotifications'])->name('admin.notification.getlist');
+Route::get('notification/getlist', 'App\Http\Controllers\Backend\NotificationController@ajaxNotifications')->name('admin.notification.getlist');
 
-Route::get('notification/clearcurrentnotifications', [NotificationController::class, 'clearCurrentNotifications'])->name('admin.notification.clearcurrentnotifications');
+Route::get('notification/clearcurrentnotifications', 'App\Http\Controllers\Backend\NotificationController@clearCurrentNotifications')->name('admin.notification.clearcurrentnotifications');
 
 Route::group(['prefix' => 'notification/{id}', 'where' => ['id' => '[0-9]+']], function () {
-    Route::get('mark/{is_read}', [NotificationController::class, 'mark'])->name('admin.notification.mark')->where(['is_read' => '[0,1]']);
+    Route::get('mark/{is_read}', 'App\Http\Controllers\Backend\NotificationController@mark')->name('admin.notification.mark')->where(['is_read' => '[0,1]']);
 });
