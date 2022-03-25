186ba4662a4748ec8519769513d474b904df1c88
Kosu <sebastian@studiograficzne.com>
2022-03-25 10:55:25 +0100

    commit

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -18,10 +18,9 @@
 
                         <div class="dropdown-menu dropdown-menu-right dropdown-danger" aria-labelledby="dropdownMenuButton">
                             @permission('view-backend')
-                            <a href="{{ route('admin.index') }}" class="dropdown-item"><i class="fas fa-tools"></i>{{ trans('navs.frontend.user.administration') }}</a>
+                            <a href="{{ route('admin.index') }}" class="dropdown-item"><i class="fas fa-tools"></i>Panel Administracyjny</a>
                             @endauth
-                            <a href="{{ route('user.account') }}" class="dropdown-item"><i class="fas fa-user"></i>{{ trans('navs.frontend.user.account') }}</a>
-                            <a href="{{ route('auth.logout') }}" class="dropdown-item"><i class="fas fa-sign-out-alt"></i>{{ trans('navs.general.logout') }}</a>
+                            <a href="{{ route('auth.logout') }}" class="dropdown-item"><i class="fas fa-sign-out-alt"></i>Wyloguj</a>
                         </div>
                     </li>
                 @endif
