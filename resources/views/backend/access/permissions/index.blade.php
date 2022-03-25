7a89468f75b029ca4568b35ab1fe34d57f944828
Kosu <sebastian@studiograficzne.com>
2022-03-25 09:58:32 +0100

    commit

--- resources/views/backend/access/permissions/index.blade.php
+++ resources/views/backend/access/permissions/index.blade.php
@@ -72,7 +72,7 @@
                         </div>
                     </div>
                     <div id="collapseBody" class="card-body">
-                        {{-- {!! history()->renderType('Uprawnienie') !!} --}}
+                        {!! history()->renderType('Uprawnienie') !!}
                     </div><!-- /.box-body -->
                 </div><!--box box-info-->
             </div>
