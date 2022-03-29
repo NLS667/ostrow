5bcd172993c8665ca987ecd054ba6ccea3a43aba
Kosu <sebastian@studiograficzne.com>
2022-03-29 18:14:18 +0200

    commit

---
+++ app/Http/Requests/Backend/Service/DeleteServiceRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Backend\Service;
+
+use App\Http\Requests\Request;
+
+/**
+ * Class DeleteServiceRequest.
+ */
+class DeleteServiceRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return access()->allow('delete-service');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
