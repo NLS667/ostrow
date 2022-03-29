dc28f2deab676cba7be598cedd900c4556e3e014
Kosu <sebastian@studiograficzne.com>
2022-03-29 13:34:36 +0200

    commit

---
+++ app/Models/Task/Task.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models\Task;
+
+use App\Models\Task\Traits\Attribute\TaskAttribute;
+use App\Models\Task\Traits\Relationship\TaskRelationship;
+use App\Models\BaseModel;
+
+/**
+ * Class Client.
+ */
+class Client extends BaseModel
+{
+    use TaskAttribute,
+        TaskRelationship;
+        
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'created_by',
+        'updated_by',
+    ];
+
+    /**
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * @param array $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->table = config('task.tasks_table');
+    }
+}
---
+++ config/task.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+	/*
+     * Services table used to store services
+     */
+    'tasks_table' => 'tasks',
+
+    /*
+     * service_client table used to save assigned services to the database.
+     */
+    'service_client_table' => 'service_client',
+];
