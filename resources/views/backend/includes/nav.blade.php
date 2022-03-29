81be055ed35f41c59eefb17fcd71c7eaa673d64e
Kosu <sebastian@studiograficzne.com>
2022-03-29 18:55:32 +0200

    commit

--- resources/views/backend/includes/nav.blade.php
+++ resources/views/backend/includes/nav.blade.php
@@ -24,7 +24,7 @@
         <li class="nav-item dropdown">
           <a class="nav-link" id="navbarDropdownMenuLink" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">
             <i class="material-icons">messages</i>
-            <span class="notification notification-counter">0</span>
+            <span class="message messages-counter">0</span>
           </a>
           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
             <span class="dropdown-header">{{ trans_choice('strings.backend.general.you_have.messages', 0, ['number' => 0]) }}</span>
@@ -34,7 +34,7 @@
         <li class="nav-item dropdown notifications-menu">
           <a class="nav-link" id="navbarDropdownMenuLink" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">
             <i class="material-icons">notifications</i>
-            <span class="notification">0</span>
+            <span class="notification notifications-counter">0</span>
           </a>
           <div class="dropdown-menu dropdown-menu-right notification-menu-container" aria-labelledby="navbarDropdownMenuLink">
             <span class="dropdown-header">{{ trans_choice('strings.backend.general.you_have.notifications', 0, ['number' => 0]) }}</span>
