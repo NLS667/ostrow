cf5eaacd6a2f57cbab64993b0f138b2898de418a
Kosu <sebastian@studiograficzne.com>
2022-03-30 17:32:18 +0200

    commit

--- resources/views/backend/client/create.blade.php
+++ resources/views/backend/client/create.blade.php
@@ -68,7 +68,7 @@
                 <div class="col-md-6">
                   <div class="row">
                     {{-- Address Street --}}
-                    <div class="col-sm-6 form-group bmd-form-group {{ $errors->has('adr_street') ? ' has-danger' : '' }}">
+                    <div class="col-sm-8 form-group bmd-form-group {{ $errors->has('adr_street') ? ' has-danger' : '' }}">
                       <label class="bmd-label-floating">Ulica</label>
                       <input class="form-control" name="adr_street" id="input-adr_street" type="text" value="{{ old('adr_street') }}" />
                       @if ($errors->has('adr_street'))
@@ -76,16 +76,18 @@
                       <span id="adr_street-error" class="error text-danger" for="input-adr_street">{{ $errors->first('adr_street') }}</span>
                       @endif
                     </div>
-                    <div class="col-sm-3 form-group bmd-form-group {{ $errors->has('adr_street_nr') ? ' has-danger' : '' }}">
-                      <label class="bmd-label-floating">Numer</label>
+                    {{-- Address Street Number--}}
+                    <div class="col-sm-2 form-group bmd-form-group {{ $errors->has('adr_street_nr') ? ' has-danger' : '' }}">
+                      <label class="bmd-label-floating">Numer domu</label>
                       <input class="form-control" name="adr_street_nr" id="input-adr_street_nr" type="text" value="{{ old('adr_street_nr') }}" />
                       @if ($errors->has('adr_street_nr'))
                       <span class="material-icons form-control-feedback">clear</span>
                       <span id="adr_street_nr-error" class="error text-danger" for="input-adr_street_nr">{{ $errors->first('adr_street_nr') }}</span>
                       @endif
                     </div>
-                    <div class="col-sm-3 form-group bmd-form-group {{ $errors->has('adr_home_nr') ? ' has-danger' : '' }}">
-                      <label class="bmd-label-floating">Numer domu/mieszkania</label>
+                    {{-- Address Home Number--}}
+                    <div class="col-sm-2 form-group bmd-form-group {{ $errors->has('adr_home_nr') ? ' has-danger' : '' }}">
+                      <label class="bmd-label-floating">Numer mieszkania</label>
                       <input class="form-control" name="adr_home_nr" id="input-adr_home_nr" type="text" value="{{ old('adr_home_nr') }}" />
                       @if ($errors->has('adr_home_nr'))
                       <span class="material-icons form-control-feedback">clear</span>
@@ -93,192 +95,50 @@
                       @endif
                     </div>
                   </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="card ">
-            <div class="card-header card-header-icon card-header-primary d-flex justify-content-between align-items-center">
-              <div class="col-md-6">
-                <div class="card-icon">
-                  <i class="material-icons">face</i>
-                </div>
-                <h4 class="card-title">Dodaj Klienta</h4>
-              </div>
-              <a href="{{ route('admin.client.index') }}" class="btn btn-sm btn-primary">Powrót do listy</a>              
-            </div>
-            <div class="card-body ">              
-              <div class="row">
-                {{-- First Name --}}
-                <div class="col-sm-6">
-                  <div class="form-group bmd-form-group {{ $errors->has('first_name') ? ' has-danger' : '' }}">
-                    <label class="bmd-label-floating">Imię</label>
-                    <input class="form-control" name="first_name" id="input-first_name" type="text" value="{{ old('first_name') }}" />
-                    @if ($errors->has('first_name'))
-                    <span class="material-icons form-control-feedback">clear</span>
-                    <span id="first_name-error" class="error text-danger" for="input-first_name">{{ $errors->first('first_name') }}</span>
-                    @endif
-                  </div>
-                </div>
-                {{-- Last Name --}}
-                <div class="col-sm-6">
                   <div class="row">
-                    <label class="col-sm-2 col-form-label">Nazwisko</label>
-                    <div class="col-sm-10">
-                      <div class="form-group{{ $errors->has('last_name') ? ' has-danger' : '' }}">
-                        <input class="form-control" name="last_name" id="input-last_name" type="text" placeholder="Nazwisko" value="{{ old('last_name') }}" />
-                        @if ($errors->has('last_name'))
-                        <span class="material-icons form-control-feedback">clear</span>
-                        <span id="last_name-error" class="error text-danger" for="input-last_name">{{ $errors->first('last_name') }}</span>
-                        @endif
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                {{-- Address Street --}}
-                <div class="col-sm-6">
-                  <div class="row">
-                    
-                  </div>
-                </div>
-                {{-- Address Street Number--}}
-                <div class="col-sm-3">
-                  <div class="row">
-                    
-                  </div>
-                </div>
-                {{-- Address Home Number--}}
-                <div class="col-sm-3">
-                  <div class="row">
-                    
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-sm-6">
-                  <div class="row">
-                    <div class="col-sm-6">
-                      <div class="row">
-                        <label class="col-sm-4 col-form-label">Kod Pocztowy</label>
-                        <div class="col-sm-8">
-                          <div class="form-group{{ $errors->has('adr_zipcode') ? ' has-danger' : '' }}">
-                            <input class="form-control" name="adr_zipcode" id="input-adr_zipcode" type="text" placeholder="kod pocztowy" value="{{ old('adr_zipcode') }}" />
-                            @if ($errors->has('adr_zipcode'))
-                            <span class="material-icons form-control-feedback">clear</span>
-                            <span id="adr_zipcode-error" class="error text-danger" for="input-adr_zipcode">{{ $errors->first('adr_zipcode') }}</span>
-                            @endif
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-sm-6">
-                      <div class="row">
-                        <label class="col-sm-4 col-form-label">Miasto</label>
-                        <div class="col-sm-8">
-                          <div class="form-group{{ $errors->has('adr_city') ? ' has-danger' : '' }}">
-                            <input class="form-control" name="adr_city" id="input-adr_city" type="text" placeholder="miasto" value="{{ old('adr_city') }}" />
-                            @if ($errors->has('adr_city'))
-                            <span class="material-icons form-control-feedback">clear</span>
-                            <span id="adr_city-error" class="error text-danger" for="input-adr_city">{{ $errors->first('adr_city') }}</span>
-                            @endif
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-sm-6">
-                  <div class="row">
-                    <div class="col-sm-6">
-                      <div class="row">
-                        <label class="col-sm-4 col-form-label">Woj.</label>
-                        <div class="col-sm-8">
-                          <div class="form-group{{ $errors->has('adr_region') ? ' has-danger' : '' }}">
-                            <input class="form-control" name="adr_region" id="input-adr_region" type="text" placeholder="województwo" value="{{ old('adr_region') }}" />
-                            @if ($errors->has('adr_region'))
-                            <span class="material-icons form-control-feedback">clear</span>
-                            <span id="adr_region-error" class="error text-danger" for="input-adr_region">{{ $errors->first('adr_region') }}</span>
-                            @endif
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-sm-6">
-                      <div class="row">
-                        <label class="col-sm-4 col-form-label">Kraj</label>
-                        <div class="col-sm-8">
-                          <div class="form-group{{ $errors->has('adr_country') ? ' has-danger' : '' }}">
-                            <input class="form-control" name="adr_country" id="input-adr_country" type="text" placeholder="kraj" value="{{ old('adr_country') }}"/>
-                            @if ($errors->has('adr_country'))
-                            <span class="material-icons form-control-feedback">clear</span>
-                            <span id="adr_country-error" class="error text-danger" for="input-adr_country">{{ $errors->first('adr_country') }}</span>
-                            @endif
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                {{-- Email --}}
-                <div class="col-sm-6">
-                  <div class="row">
-                    <label class="col-sm-2 col-form-label">E-mail</label>
-                    <div class="col-sm-10">
-                      <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
-                        <input class="form-control" name="email" id="input-email" type="text" placeholder="E-mail" value="{{ old('email') }}"/>
-                        @if ($errors->has('email'))
-                        <span class="material-icons form-control-feedback">clear</span>
-                        <span id="email-error" class="error text-danger" for="input-email">{{ $errors->first('email') }}</span>
-                        @endif
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                {{-- Phone --}}
-                <div class="col-sm-6">
-                  <div class="row">
-                    <label class="col-sm-2 col-form-label">Nr telefonu</label>
-                    <div class="col-sm-10">
-                      <div class="form-group{{ $errors->has('phone_nr') ? ' has-danger' : '' }}">
-                        <input class="form-control" name="phone_nr" id="input-phone_nr" type="text" placeholder="Nr telefonu" value="{{ old('phone_nr') }}"/>
-                        @if ($errors->has('phone_nr'))
-                        <span class="material-icons form-control-feedback">clear</span>
-                        <span id="phone_nr-error" class="error text-danger" for="input-phone_nr">{{ $errors->first('phone_nr') }}</span>
-                        @endif
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>     
-              
-              {{-- Associated Services --}}
-              <div class="row">
-                <label class="col-sm-1 col-form-label" for="status">Przypisane Usługi</label>
-                <div class="col-sm-11">
-                  <div class="form-group">
-                      @if (count($services) > 0)
-                      <select name="associated_services[]" class="form-control select2" multiple="multiple">
-                      @foreach($services as $service)
-                        <option value="{{ $service->id }}">{{ $service->name }}</option>
-                      @endforeach
-                      </select>
-                      @else
-                      Brak przypisanych Usług.
+
+                    <div class="col-sm-4 form-group bmd-form-group {{ $errors->has('adr_zipcode') ? ' has-danger' : '' }}">
+                      <label class="bmd-label-floating">Kod Pocztowy</label>
+                      <input class="form-control" name="adr_zipcode" id="input-adr_zipcode" type="text" value="{{ old('adr_zipcode') }}" />
+                      @if ($errors->has('adr_zipcode'))
+                      <span class="material-icons form-control-feedback">clear</span>
+                      <span id="adr_zipcode-error" class="error text-danger" for="input-adr_zipcode">{{ $errors->first('adr_zipcode') }}</span>
                       @endif
-                  </div><!--form control-->
+                    </div>
+
+                    <div class="col-sm-8 form-group bmd-form-group {{ $errors->has('adr_city') ? ' has-danger' : '' }}">
+                      <label class="bmd-label-floating">Miasto</label>
+                      <input class="form-control" name="adr_city" id="input-adr_city" type="text" value="{{ old('adr_city') }}" />
+                      @if ($errors->has('adr_city'))
+                      <span class="material-icons form-control-feedback">clear</span>
+                      <span id="adr_city-error" class="error text-danger" for="input-adr_city">{{ $errors->first('adr_city') }}</span>
+                      @endif
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-sm-6 form-group bmd-form-group {{ $errors->has('adr_region') ? ' has-danger' : '' }}">
+                      <label class="bmd-label-floating">Województwo</label>
+                      <input class="form-control" name="adr_region" id="input-adr_region" type="text" value="{{ old('adr_region') }}" />
+                      @if ($errors->has('adr_region'))
+                      <span class="material-icons form-control-feedback">clear</span>
+                      <span id="adr_region-error" class="error text-danger" for="input-adr_region">{{ $errors->first('adr_region') }}</span>
+                      @endif
+                    </div>
+
+                    <div class="col-sm-6 form-group bmd-form-group {{ $errors->has('adr_country') ? ' has-danger' : '' }}">
+                      <label class="bmd-label-floating">Kraj</label>
+                      <input class="form-control" name="adr_country" id="input-adr_country" type="text" value="{{ old('adr_country') }}"/>
+                      @if ($errors->has('adr_country'))
+                      <span class="material-icons form-control-feedback">clear</span>
+                      <span id="adr_country-error" class="error text-danger" for="input-adr_country">{{ $errors->first('adr_country') }}</span>
+                      @endif
+                    </div>
+                  </div>
                 </div>
-              </div> 
-              <div class="card-footer">
-                <button type="submit" class="btn btn-fill btn-success">Dodaj</button>
               </div>
             </div>
           </div>
-          </form>
-          {{ $errors }}
+        </form>
       </div>
     </div>
   </div>
