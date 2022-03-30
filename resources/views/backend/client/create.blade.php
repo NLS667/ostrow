82c3960a9215794b1b66a4df69ee7fc8cb887425
Kosu <sebastian@studiograficzne.com>
2022-03-30 11:29:24 +0200

    commit

--- resources/views/backend/client/create.blade.php
+++ resources/views/backend/client/create.blade.php
@@ -22,13 +22,30 @@
             <div class="card-body ">
               {{-- First Name --}}
               <div class="row">
-                <label class="col-sm-2 col-form-label">Imię</label>
-                <div class="col-sm-7">
-                  <div class="form-group{{ $errors->has('first_name') ? ' has-danger' : '' }}">
-                    <input class="form-control{{ $errors->has('first_name') ? ' is-invalid' : '' }}" name="first_name" id="input-first_name" type="text" placeholder="Imię" value="{{ old('first_name') }}" required="true" aria-required="true"/>
-                    @if ($errors->has('first_name'))
-                    <span id="name-error" class="error text-danger" for="input-first_name">{{ $errors->first('first_name') }}</span>
-                    @endif
+                <div class="col-sm-6">
+                  <div class="row">
+                    <label class="col-sm-2 col-form-label">Imię</label>
+                    <div class="col-sm-10">
+                      <div class="form-group{{ $errors->has('first_name') ? ' has-danger' : '' }}">
+                        <input class="form-control{{ $errors->has('first_name') ? ' is-invalid' : '' }}" name="first_name" id="input-first_name" type="text" placeholder="Imię" value="{{ old('first_name') }}" required="true" aria-required="true"/>
+                        @if ($errors->has('first_name'))
+                        <span id="name-error" class="error text-danger" for="input-first_name">{{ $errors->first('first_name') }}</span>
+                        @endif
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="row">
+                    <label class="col-sm-2 col-form-label">Nazwisko</label>
+                    <div class="col-sm-7">
+                      <div class="form-group{{ $errors->has('last_name') ? ' has-danger' : '' }}">
+                        <input class="form-control{{ $errors->has('last_name') ? ' is-invalid' : '' }}" name="last_name" id="input-last_name" type="text" placeholder="Nazwisko" value="{{ old('last_name') }}" required="true" aria-required="true"/>
+                        @if ($errors->has('last_name'))
+                        <span id="name-error" class="error text-danger" for="input-last_name">{{ $errors->first('last_name') }}</span>
+                        @endif
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -63,7 +80,7 @@
                 <label class="col-sm-2 col-form-label">Nr telefonu</label>
                 <div class="col-sm-7">
                   <div class="form-group{{ $errors->has('phone_nr') ? ' has-danger' : '' }}">
-                    <input class="form-control{{ $errors->has('phone_nr') ? ' is-invalid' : '' }}" name="phone_nr" id="input-phone_nr" type="text" placeholder="nr telefonu" value="{{ old('phone_nr') }}" required />
+                    <input class="form-control{{ $errors->has('phone_nr') ? ' is-invalid' : '' }}" name="phone_nr" id="input-phone_nr" type="text" placeholder="Nr telefonu" value="{{ old('phone_nr') }}" required />
                     @if ($errors->has('phone_nr'))
                     <span id="phone_nr-error" class="error text-danger" for="input-phone_nr">{{ $errors->first('phone_nr') }}</span>
                     @endif
