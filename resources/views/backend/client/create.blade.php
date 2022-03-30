4fd4317211e2f04f664e98170ecb013e53c8497c
Kosu <sebastian@studiograficzne.com>
2022-03-30 11:48:29 +0200

    commit

--- resources/views/backend/client/create.blade.php
+++ resources/views/backend/client/create.blade.php
@@ -19,9 +19,9 @@
               </div>
               <a href="{{ route('admin.client.index') }}" class="btn btn-sm btn-primary">Powrót do listy</a>              
             </div>
-            <div class="card-body ">
-              {{-- First Name --}}
+            <div class="card-body ">              
               <div class="row">
+                {{-- First Name --}}
                 <div class="col-sm-6">
                   <div class="row">
                     <label class="col-sm-2 col-form-label">Imię</label>
@@ -29,12 +29,13 @@
                       <div class="form-group{{ $errors->has('first_name') ? ' has-danger' : '' }}">
                         <input class="form-control{{ $errors->has('first_name') ? ' is-invalid' : '' }}" name="first_name" id="input-first_name" type="text" placeholder="Imię" value="{{ old('first_name') }}" required="true" aria-required="true"/>
                         @if ($errors->has('first_name'))
-                        <span id="name-error" class="error text-danger" for="input-first_name">{{ $errors->first('first_name') }}</span>
+                        <span id="first_name-error" class="error text-danger" for="input-first_name">{{ $errors->first('first_name') }}</span>
                         @endif
                       </div>
                     </div>
                   </div>
                 </div>
+                {{-- Last Name --}}
                 <div class="col-sm-6">
                   <div class="row">
                     <label class="col-sm-2 col-form-label">Nazwisko</label>
@@ -42,7 +43,7 @@
                       <div class="form-group{{ $errors->has('last_name') ? ' has-danger' : '' }}">
                         <input class="form-control{{ $errors->has('last_name') ? ' is-invalid' : '' }}" name="last_name" id="input-last_name" type="text" placeholder="Nazwisko" value="{{ old('last_name') }}" required="true" aria-required="true"/>
                         @if ($errors->has('last_name'))
-                        <span id="name-error" class="error text-danger" for="input-last_name">{{ $errors->first('last_name') }}</span>
+                        <span id="last_name-error" class="error text-danger" for="input-last_name">{{ $errors->first('last_name') }}</span>
                         @endif
                       </div>
                     </div>
@@ -50,40 +51,71 @@
                 </div>
               </div>
               <div class="row">
-                <div class="col-sm-8">
+                {{-- Address Street --}}
+                <div class="col-sm-6">
                   <div class="row">
                     <label class="col-sm-2 col-form-label">Ulica</label>
                     <div class="col-sm-10">
                       <div class="form-group{{ $errors->has('adr_street') ? ' has-danger' : '' }}">
                         <input class="form-control{{ $errors->has('adr_street') ? ' is-invalid' : '' }}" name="adr_street" id="input-adr_street" type="text" placeholder="ulica" value="{{ old('adr_street') }}" required="true" aria-required="true"/>
                         @if ($errors->has('adr_street'))
-                        <span id="name-error" class="error text-danger" for="input-adr_street">{{ $errors->first('adr_street') }}</span>
+                        <span id="adr_street-error" class="error text-danger" for="input-adr_street">{{ $errors->first('adr_street') }}</span>
                         @endif
                       </div>
                     </div>
                   </div>
                 </div>
-                <div class="col-sm-2">
+                {{-- Address Street Number--}}
+                <div class="col-sm-3">
                   <div class="row">
-                    <label class="col-sm-2 col-form-label">Numer</label>
-                    <div class="col-sm-10">
+                    <label class="col-sm-4 col-form-label">Numer</label>
+                    <div class="col-sm-8">
                       <div class="form-group{{ $errors->has('adr_street_nr') ? ' has-danger' : '' }}">
                         <input class="form-control{{ $errors->has('adr_street_nr') ? ' is-invalid' : '' }}" name="adr_street_nr" id="input-adr_street_nr" type="text" placeholder="numer" value="{{ old('adr_street_nr') }}" required="true" aria-required="true"/>
                         @if ($errors->has('adr_street_nr'))
-                        <span id="name-error" class="error text-danger" for="input-adr_street_nr">{{ $errors->first('adr_street_nr') }}</span>
+                        <span id="adr_street_nr-error" class="error text-danger" for="input-adr_street_nr">{{ $errors->first('adr_street_nr') }}</span>
                         @endif
                       </div>
                     </div>
                   </div>
                 </div>
-                <div class="col-sm-2">
+                {{-- Address Home Number--}}
+                <div class="col-sm-3">
                   <div class="row">
-                    <label class="col-sm-2 col-form-label">Numer domu/mieszkania</label>
-                    <div class="col-sm-10">
+                    <label class="col-sm-6 col-form-label">Numer domu/mieszkania</label>
+                    <div class="col-sm-6">
                       <div class="form-group{{ $errors->has('adr_home_nr') ? ' has-danger' : '' }}">
                         <input class="form-control{{ $errors->has('adr_home_nr') ? ' is-invalid' : '' }}" name="adr_home_nr" id="input-adr_home_nr" type="text" placeholder="numer domu" value="{{ old('adr_home_nr') }}" required="true" aria-required="true"/>
                         @if ($errors->has('adr_home_nr'))
-                        <span id="name-error" class="error text-danger" for="input-adr_home_nr">{{ $errors->first('adr_home_nr') }}</span>
+                        <span id="adr_home_nr-error" class="error text-danger" for="input-adr_home_nr">{{ $errors->first('adr_home_nr') }}</span>
+                        @endif
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-6">
+                  <div class="row">
+                    <label class="col-sm-4 col-form-label">Kod Pocztowy</label>
+                    <div class="col-sm-8">
+                      <div class="form-group{{ $errors->has('adr_zipcode') ? ' has-danger' : '' }}">
+                        <input class="form-control{{ $errors->has('adr_zipcode') ? ' is-invalid' : '' }}" name="adr_zipcode" id="input-adr_zipcode" type="text" placeholder="kod pocztowy" value="{{ old('adr_zipcode') }}" required="true" aria-required="true"/>
+                        @if ($errors->has('adr_zipcode'))
+                        <span id="adr_zipcode-error" class="error text-danger" for="input-adr_zipcode">{{ $errors->first('adr_zipcode') }}</span>
+                        @endif
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="row">
+                    <label class="col-sm-4 col-form-label">Miasto</label>
+                    <div class="col-sm-8">
+                      <div class="form-group{{ $errors->has('adr_city') ? ' has-danger' : '' }}">
+                        <input class="form-control{{ $errors->has('adr_city') ? ' is-invalid' : '' }}" name="adr_city" id="input-adr_city" type="text" placeholder="miasto" value="{{ old('adr_city') }}" required="true" aria-required="true"/>
+                        @if ($errors->has('adr_city'))
+                        <span id="adr_city-error" class="error text-danger" for="input-adr_city">{{ $errors->first('adr_city') }}</span>
                         @endif
                       </div>
                     </div>
