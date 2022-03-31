77352ebdcf2df9ab1056a24623ac89c7f672f4a0
Kosu <sebastian@studiograficzne.com>
2022-03-31 11:47:21 +0200

    commit

--- resources/assets/js/plugins/common.js
+++ resources/assets/js/plugins/common.js
@@ -481,7 +481,7 @@ var Backend = {}; // common variable used in all the files of the backend
         Clients: {
             selectors: {
                 select2: $(".select2"),
-                addService: document.querySelector("select[name='add-service']",
+                addService: document.querySelector("select[name='add-service']"),
             },
             init: function (page) {
                 this.setSelectors();
