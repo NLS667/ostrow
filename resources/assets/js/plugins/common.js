dca6122177d18b8d21e7cd03ba754cf60936ef1c
Kosu <sebastian@studiograficzne.com>
2022-03-31 18:42:07 +0200

    commit

--- resources/assets/js/plugins/common.js
+++ resources/assets/js/plugins/common.js
@@ -502,10 +502,10 @@ var Backend = {}; // common variable used in all the files of the backend
                             
                             if(!$('#serviceTab_' + id ).length){
                                 $(".add-service").closest('li').before('<li id="serviceTab_' + id + '" class="nav-item"><a href="#service_' + id + '" class="nav-link tab" data-toggle="tab" role="tablist">' + name + '</a> <span class="material-icons">clear</span></li>');
-                                $.get("admin.client.getserviceform", function(data){
+                                $.get("getserviceform", function(data){
                                     $('.tab-content').append(data);
                                 });
-                                $('.tab-content').append('<div class="tab-pane" id="' + tabId + '">@include("backend.includes.partials.client-add-service")</div>');
+                                //$('.tab-content').append('<div class="tab-pane" id="' + tabId + '">@include("backend.includes.partials.client-add-service")</div>');
                                 $('.services li:nth-child(' + count + ') a').click();
                             }
                         }
