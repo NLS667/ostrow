caef3ff123bb80e17d88e0849a4f8e8effceaece
Kosu <sebastian@studiograficzne.com>
2022-03-31 19:46:59 +0200

    commit

--- app/Http/Controllers/Backend/Client/ClientController.php
+++ app/Http/Controllers/Backend/Client/ClientController.php
@@ -140,7 +140,7 @@ class ClientController extends Controller
      */
     public function getServiceForm(Request $request)
     {
-        $service = $this->services->find($request->service)->first();
+        $service = $this->services->find($request->service);
         return view('backend.includes.partials.client-add-service', ['service' => $service]);
     }
 }
