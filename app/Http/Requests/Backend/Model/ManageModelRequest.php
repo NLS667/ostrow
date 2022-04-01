b71df1a6bf5e68b099bdaa86dff37e53f7780ded
Kosu <sebastian@studiograficzne.com>
2022-04-01 16:35:44 +0200

    commit

---
+++ app/Http/Requests/Backend/Model/ManageModelRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Backend\Model;
+
+use App\Http\Requests\Request;
+
+/**
+ * Class ManageModelRequest.
+ */
+class ManageModelRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return access()->allow('view-model-management');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
