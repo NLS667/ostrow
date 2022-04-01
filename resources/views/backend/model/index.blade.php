ed8f30d1c0ee40cf788f20350b263d47e18bc9ae
Kosu <sebastian@studiograficzne.com>
2022-04-01 16:39:07 +0200

    commit

---
+++ resources/views/backend/model/index.blade.php
@@ -0,0 +1,123 @@
+@extends('backend.layouts.app', ['activePage' => 'model-management', 'titlePage' => 'Zarządzanie Modelami'])
+
+@section('content')
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12"> 
+                <div class="card">
+                    <div class="card-header card-header-icon card-header-info d-flex justify-content-between align-items-center">
+                        <div class="col-md-6">
+                            <div class="card-icon">
+                                <i class="material-icons">inventory</i>
+                            </div>
+                            <h4 class="card-title">Wszystkie Modele</h4>
+                        </div>
+                        <div class="card-tools">
+                            @include('backend.includes.partials.model-header-buttons')
+                        </div>
+                    </div><!-- /.card-header --> 
+
+                    <div class="card-body">
+                        <div class="table-responsive data-table-wrapper">
+                            <table id="models-table" class="table dataTable table-striped table-hover table-no-bordered dtr-inline" cellspacing="0" width="100%" style="width: 100%;">
+                                <thead>
+                                    <tr role="row">
+                                        <th>Nazwa</th>
+                                        <th>Opis</th>
+                                        <th>Producent</th>
+                                        <th>Utworzony</th>
+                                        <th>Zmieniony</th>
+                                        <th>Akcje</th>
+                                    </tr>
+                                </thead>
+                                <tfoot>
+                                    <tr>
+                                        <th>Nazwa</th>
+                                        <th>Opis</th>
+                                        <th>Producent</th>
+                                        <th>Utworzony</th>
+                                        <th>Zmieniony</th>
+                                        <th class="text-right">Akcje</th>
+                                    </tr>
+                                </tfoot>
+                                <thead class="transparent-bg">
+                                    <tr>
+                                        <th>
+                                            <div class="input-group position-relative">
+                                                {!! Form::text('name', null, ["class" => "search-input-text form-control", "data-column" => 0, "placeholder" => 'Nazwa']) !!}
+                                                <span class="form-clear d-none reset-data"><i class="material-icons">clear</i></span>
+                                            </div>
+                                        </th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div><!--table-responsive-->
+                    </div><!-- /.card-body -->
+                </div><!--card-->
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('after-scripts')
+    {{-- For DataTables --}}
+
+    <script>
+        $(function() {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+
+            var dataTable = $('#models-table').dataTable({
+                columnDefs: [
+                    {"className": "dt-center", "targets": "_all"}
+                ],
+                processing: true,
+                serverSide: true,
+                searching: true,
+
+                ajax: {
+                    url: '{{ route("admin.models.get") }}',
+                    type: 'post',
+                    data: {status: 1, trashed: false}
+                },
+                columns: [
+                    {data: 'name', name: 'models.name'},
+                    {data: 'description', name: 'models.description'},
+                    {data: 'producer', name: 'models.producer'},
+                    {data: 'created_at', name: 'models.created_at'},
+                    {data: 'updated_at', name: 'models.updated_at'},
+                    {data: 'actions', name: 'actions', className: 'text-center', searchable: false, sortable: false}
+                ],
+                order: [[3, "asc"]],
+                searchDelay: 500,
+                dom: "<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'f>><'row'<'col-sm-12'tr>><'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+                buttons: {
+                    buttons: [
+                        { extend: 'copy', className: 'copyButton',  exportOptions: {columns: [ 0, 1, 2, 3, 4 ]  }},
+                        { extend: 'csv', className: 'csvButton',  exportOptions: {columns: [ 0, 1, 2, 3, 4 ]  }},
+                        { extend: 'excel', className: 'excelButton',  exportOptions: {columns: [ 0, 1, 2, 3, 4 ]  }},
+                        { extend: 'pdf', className: 'pdfButton',  exportOptions: {columns: [ 0, 1, 2, 3, 4 ]  }},
+                        { extend: 'print', className: 'printButton',  exportOptions: {columns: [ 0, 1, 2, 3, 4 ]  }}
+                    ]
+                },
+                language: {
+                    @lang('datatable.strings')
+                }
+            });
+
+            Backend.DataTableSearch.init(dataTable);
+            bootstrapClearButton();
+    
+        });
+    </script>
+@endsection
