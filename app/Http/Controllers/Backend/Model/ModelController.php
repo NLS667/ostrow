105fbf238bd417014eb85875f106ad7fb76e0b3f
Kosu <sebastian@studiograficzne.com>
2022-04-01 13:17:25 +0200

    commit

---
+++ app/Http/Controllers/Backend/Model/ModelController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Model;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Backend\Model\CreateModelRequest;
+use App\Http\Requests\Backend\Model\DeleteModelRequest;
+use App\Http\Requests\Backend\Model\EditModelRequest;
+use App\Http\Requests\Backend\Model\ManageModelrRequest;
+use App\Http\Requests\Backend\Model\StoreModelRequest;
+use App\Http\Requests\Backend\Model\UpdateModelRequest;
+use App\Http\Responses\Backend\Model\EditResponse;
+use App\Http\Responses\RedirectResponse;
+use App\Http\Responses\ViewResponse;
+use App\Models\Model\Model;
+use App\Repositories\Backend\Model\ModelRepository;
+
+/**
+ * Class ModelController.
+ */
+class ModelController extends Controller
+{
+    /**
+     * @var \App\Repositories\Backend\Model\ModelRepository
+     */
+    protected $model;
+
+    /**
+     * @param \App\Repositories\Backend\Model\ModelRepository $model
+     */
+    public function __construct(ModelRepository $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * @param \App\Http\Requests\Backend\Model\ManageModelRequest $request
+     *
+     * @return \App\Http\Responses\ViewResponse
+     */
+    public function index(ManageModelRequest $request)
+    {
+        return new ViewResponse('backend.model.index');
+    }
+
+    /**
+     * @param \App\Http\Requests\Backend\Model\CreateModelRequest $request
+     *
+     * @return \App\Http\Responses\ViewResponse
+     */
+    public function create(CreateModelRequest $request)
+    {
+        return new ViewResponse('backend.model.create');
+    }
+
+    /**
+     * @param \App\Http\Requests\Backend\Model\StoreModelRequest $request
+     *
+     * @return mixed
+     */
+    public function store(StoreModelRequest $request)
+    {
+        $this->model->create($request->all());
+
+        return new RedirectResponse(route('admin.model.index'), ['flash_success' => trans('alerts.backend.models.created')]);
+    }
+
+    /**
+     * @param \App\Models\Model\Model                           $model
+     * @param \App\Http\Requests\Backend\Model\EditModelRequest $request
+     *
+     * @return \App\Http\Responses\Backend\Model\EditResponse
+     */
+    public function edit(Model $model, EditModelRequest $request)
+    {
+        return new EditResponse($model);
+    }
+
+    /**
+     * @param \App\Models\Access\Model\Model                      $model
+     * @param \App\Http\Requests\Backend\Model\UpdateModelRequest $request
+     *
+     * @return \App\Http\Responses\RedirectResponse
+     */
+    public function update(Model $model, UpdateModelRequest $request)
+    {
+        $this->model->update($model, $request->all());
+
+        return new RedirectResponse(route('admin.model.index'), ['flash_success' => trans('alerts.backend.models.updated')]);
+    }
+
+    /**
+     * @param \App\Models\Access\Model\Model                      $model
+     * @param \App\Http\Requests\Backend\Model\DeleteModelRequest $request
+     *
+     * @return \App\Http\Responses\RedirectResponse
+     */
+    public function destroy(Model $model, DeleteModelRequest $request)
+    {
+        $this->model->delete($model);
+
+        return new RedirectResponse(route('admin.model.index'), ['flash_success' => trans('alerts.backend.models.deleted')]);
+    }
+}
---
+++ app/Http/Controllers/Backend/Model/ModelTableController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Model;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Backend\Model\ManageModelRequest;
+use App\Repositories\Backend\Model\ModelRepository;
+use Carbon\Carbon;
+use Yajra\DataTables\Facades\DataTables;
+
+/**
+ * Class ModelTableController.
+ */
+class ModelTableController extends Controller
+{
+    protected $models;
+
+    /**
+     * @param \App\Repositories\Backend\Model\ModelRepository $service
+     */
+    public function __construct(ModelRepository $models)
+    {
+        $this->models = $models;
+    }
+
+    /**
+     * @param \App\Http\Requests\Backend\Model\ManageModelRequest $request
+     *
+     * @return mixed
+     */
+    public function __invoke(ManageModelRequest $request)
+    {
+        return Datatables::make($this->models->getForDataTable())
+            ->escapeColumns(['name'])
+            ->addColumn('description', function ($model) {
+                return $model->description;
+            })
+            ->addColumn('producer', function ($user) {
+                return $model->producer;
+            })
+            ->addColumn('created_at', function ($model) {
+                return Carbon::parse($model->created_at)->toDateString();
+            })
+            ->addColumn('updated_at', function ($model) {
+                if(isset($model->updated_at))
+                {
+                    return Carbon::parse($model->updated_at)->toDateString();                    
+                } else {
+                    return 'Nigdy';
+                }
+            })
+            ->addColumn('actions', function ($model) {
+                return $model->action_buttons;
+            })
+            ->make(true);
+    }
+}
