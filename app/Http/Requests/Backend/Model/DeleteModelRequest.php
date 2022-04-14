ba0f0de955638a8a46d630014986f2360f0d14b5
NLS667 <kosu@uroczysko.org>
2022-04-14 12:46:33 +0200

    Commit

---
+++ app/Http/Requests/Backend/Model/DeleteModelRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Backend\Model;
+
+use App\Http\Requests\Request;
+
+/**
+ * Class DeleteModelRequest.
+ */
+class DeleteModelRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return access()->allow('delete-model');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
