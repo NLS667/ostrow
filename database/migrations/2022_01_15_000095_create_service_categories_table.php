b4bf8411cc65502abe3a8ef888c19b033ebf43a3
NLS667 <kosu@uroczysko.org>
2022-04-14 16:36:56 +0200

    Commit

--- database/migrations/2022_01_15_000095_create_service_categories_table.php
+++ database/migrations/2022_01_15_000095_create_service_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateServiceCategoryTable extends Migration
+class CreateServiceCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
