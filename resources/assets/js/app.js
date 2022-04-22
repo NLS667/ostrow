00d82808ea0a5e136487d4e51d317a1eefa4ee21
NLS667 <kosu@uroczysko.org>
2022-04-22 13:08:28 +0200

    Commit

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -26,9 +26,9 @@ require('leaflet-sleep');
 
 L.Icon.Default.imagePath = '/images/';
 L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+    iconRetinaUrl: '/images/marker-icon-2x.png',
+    iconUrl: '/images/marker-icon.png',
+    shadowUrl: '/images/marker-shadow.png',
 });
 //Import Select2
 //window.select2 = require('select2');
