0abc097d7e95818b48850b6ba2c998af4f5fa68b
NLS667 <kosu@uroczysko.org>
2022-04-22 12:38:08 +0200

    Commit

--- resources/views/backend/client/show.blade.php
+++ resources/views/backend/client/show.blade.php
@@ -9,7 +9,7 @@
         			<div class="card-header card-header-icon card-header-primary">
 	                	<div class="col-md-6">
 	                  		<div class="card-icon">
-	                    		<i class="material-icons">face</i>
+	                    		<i class="material-icons">public</i>
 	                  		</div>
 	                  		<h4 class="card-title">Mapa</h4>
 	                	</div>
@@ -20,16 +20,20 @@
           		</div>
         	</div>
         	<div class="col-md-4">
-        		<div class="card">
-        			<div class="card-header card-header-text card-header-rose">
-              			<div class="card-text">
-                			<h4 class="card-title">Dane Podstawowe</h4>
-              			</div>
+        		<div class="card card-nav-tabs text-center">
+        			<div class="card-header card-header-rose">
+                		Dane Podstawowe
             		</div>
 	            	<div class="card-body ">
-                		<div class="row">
-
-                		</div>
+                		<h4 class="card-title">{{ $client->first_name }}&nbsc;{{ $client->last_name }}</h4>
+                		<p class="card-text">
+                			{{$client->adr_street}}&nbsc;{{$client->adr_street_nr}}</br>
+                			{{$client->adr_zipcode}}&nbsc;{{$client->adr_city}}</br>
+                			{{$client->adr_region}}</br>
+                			{{$client->adr_country}}</br></br>
+                			{{$client->email}}</br>
+                			{{$client->phone_nr}}</br>
+                		</p>
                 	</div>
             	</div>
         	</div>
