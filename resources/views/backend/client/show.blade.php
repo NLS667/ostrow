ce4a759ef16c3e7a10957bbbbfcf95762142eb70
NLS667 <kosu@uroczysko.org>
2022-04-22 11:51:37 +0200

    Commit

--- resources/views/backend/client/show.blade.php
+++ resources/views/backend/client/show.blade.php
@@ -5,10 +5,34 @@
   <div class="container-fluid">
   	<div class="row">
         <div class="col-md-6">
+        	<div class="card">
+        		<div class="card-header card-header-icon card-header-primary">
+	                <div class="col-md-6">
+	                  <div class="card-icon">
+	                    <i class="material-icons">face</i>
+	                  </div>
+	                  <h4 class="card-title">Dane Podstawowe</h4>
+	                </div>
+	            </div>
+	            <div class="card-body ">
+                	<div class="row">
 
+                	</div>
+                </div>
+              </div>
+        	</div>
         </div>
         <div class="col-md-6">
-
+        	<div class="card ">
+            <div class="card-header card-header-text card-header-rose">
+              <div class="card-text">
+                <h4 class="card-title">Mapa</h4>
+              </div>
+            </div>
+            <div class="card-body ">
+              <clientsmap :data='{!! json_encode($map_data) !!}'></clientsmap>
+            </div>
+          </div>
         </div>
     </div>
   </div>
