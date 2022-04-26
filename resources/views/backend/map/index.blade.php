2fe50d79ea3808543a318cf7adfc0131cf8841ca
NLS667 <kosu@uroczysko.org>
2022-04-26 10:02:29 +0200

    Commit

--- resources/views/backend/map/index.blade.php
+++ resources/views/backend/map/index.blade.php
@@ -1,7 +1,7 @@
 @extends('backend.layouts.app', ['activePage' => 'client-map', 'titlePage' => 'Mapa Klientów'])
 
 @section('content')
-  <div class="content" style="padding:none;">
+  <div class="content" style="padding:none !important;">
     <clientsmap :data='{!! json_encode($map_data) !!}'></clientsmap>
   </div>
 @endsection
