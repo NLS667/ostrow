3ed27c142251f980b2ec6712f7f26d7cdf5cc69b
NLS667 <kosu@uroczysko.org>
2022-04-29 16:44:13 +0200

    Commit

--- webpack.mix.js
+++ webpack.mix.js
@@ -34,7 +34,7 @@ mix.autoload({
         "resources/assets/js/plugins/common.js",
         'resources/assets/js/plugins/bootstrap-material-design.min.js',
         'resources/assets/js/plugins/perfect-scrollbar.jquery.min.js',
-        'resources/assets/js/plugins/moment.min.js',
+        'resources/assets/js/plugins/moment-with-locales.min.js',
         'resources/assets/js/plugins/sweetalert2.js',
         'resources/assets/js/plugins/select2.js',
         'resources/assets/js/plugins/jquery.validate.min.js',
