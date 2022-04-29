5b81663ebbbb655765e2e9f190a375566a48769d
NLS667 <kosu@uroczysko.org>
2022-04-29 16:51:56 +0200

    Commit

--- resources/assets/js/plugins/material-dashboard.js
+++ resources/assets/js/plugins/material-dashboard.js
@@ -301,7 +301,7 @@ md = {
         clear: 'fa fa-trash',
         close: 'fa fa-remove'
       }
-    }).on('changeDate', function(e){   
+    }).on('dp.change', function(e){   
       console.log('date and time changed');
         $(this).parent().addClass('is-filled'); 
     });
