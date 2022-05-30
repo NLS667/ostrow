588d4e5ce22170ea2db8cd87d7c8ba6fabfc8833
NLS667 <kosu@uroczysko.org>
2022-05-30 11:58:27 +0200

    commit

--- resources/views/backend/client/index.blade.php
+++ resources/views/backend/client/index.blade.php
@@ -133,13 +133,13 @@
                 ],
                 order: [[5, "asc"]],
                 searchDelay: 500,
-                dom: "<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'Bf>><'row'<'col-sm-12'tr>><'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+                dom: "<'row'<'col-sm-12 col-md-4'l><'col-sm-12 col-md-4'B><'col-sm-12 col-md-4'Bf>><'row'<'col-sm-12'tr>><'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
                 buttons: {
                     buttons: [
-                        { extend: 'copy', className: 'copyButton',  exportOptions: {columns: [ 0, 1, 2, 3, 5, 6, 7 ]  }},
-                        { extend: 'csv', className: 'csvButton',  exportOptions: {columns: [ 0, 1, 2, 3, 5, 6 ,7 ]  }},
-                        { extend: 'excel', className: 'excelButton',  exportOptions: {columns: [ 0, 1, 2, 3, 5, 6, 7 ]  }},
-                        { extend: 'pdf', className: 'pdfButton',  exportOptions: {columns: [ 0, 1, 2, 3, 5, 6, 7 ]  }},
+                        { extend: 'copyHtml5', className: 'copyButton',  exportOptions: {columns: [ 0, 1, 2, 3, 5, 6, 7 ]  }},
+                        { extend: 'csvHtml5', className: 'csvButton',  exportOptions: {columns: [ 0, 1, 2, 3, 5, 6 ,7 ]  }},
+                        { extend: 'excelHtml5', className: 'excelButton',  exportOptions: {columns: [ 0, 1, 2, 3, 5, 6, 7 ]  }},
+                        { extend: 'pdfHtml5', className: 'pdfButton',  exportOptions: {columns: [ 0, 1, 2, 3, 5, 6, 7 ]  }},
                         { extend: 'print', className: 'printButton',  exportOptions: {columns: [ 0, 1, 2, 3, 5, 6, 7 ]  }}
                     ]
                 },
