bedab99f97cc9be4a5c0ab9fe0ac21d16a1ec99b
NLS667 <kosu@uroczysko.org>
2022-05-30 11:18:41 +0200

    commit

---
+++ resources/views/backend/client/deleted.blade.php
@@ -0,0 +1,149 @@
+@extends ('backend.layouts.app', ['activePage' => 'client-deactivated', 'titlePage' => 'Zarządzanie Klientami']) 
+
+@section('content')
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header card-header-icon card-header-info d-flex justify-content-between align-items-center">
+                        <div class="col-md-6">
+                            <div class="card-icon">
+                                <i class="material-icons">face</i>
+                            </div>
+                            <h4 class="card-title">Nieaktywni Klienci</h4>
+                        </div>
+                        <div class="card-tools">
+                            @include('backend.includes.partials.client-header-buttons')
+                        </div>
+                    </div><!-- /.box-header -->
+
+                    <div class="card-body">
+                        <div class="table-responsive data-table-wrapper">
+                            <table id="clients-table" class="table dataTable table-striped table-hover table-no-bordered dtr-inline" cellspacing="0" width="100%" style="width: 100%;">
+                                <thead>
+                                    <tr role="row">
+                                        <th>Imię</th>
+                                        <th>Nazwisko</th>
+                                        <th>E-mail</th>
+                                        <th>Nr telefonu</th>
+                                        <th>Aktywny?</th>
+                                        <th>Usługi</th>
+                                        <th>Utworzony</th>
+                                        <th>Zmieniony</th>
+                                        <th>Akcje</th>
+                                    </tr>
+                                </thead>
+                                <tfoot>
+                                    <tr>
+                                        <th>Imię</th>
+                                        <th>Nazwisko</th>
+                                        <th>E-mail</th>
+                                        <th>Nr telefonu</th>
+                                        <th>Aktywny?</th>
+                                        <th>Usługi</th>
+                                        <th>Utworzony</th>
+                                        <th>Zmieniony</th>
+                                        <th class="text-right">Akcje</th>
+                                    </tr>
+                                </tfoot>
+                                <thead class="transparent-bg">
+                                    <tr>
+                                        <th>
+                                            <div class="input-group position-relative">
+                                                {!! Form::text('first_name', null, ["class" => "search-input-text form-control", "data-column" => 0, "placeholder" => 'Imię']) !!}
+                                                <span class="form-clear d-none reset-data"><i class="material-icons">clear</i></span>
+                                            </div>
+                                        </th>
+                                        <th>
+                                            <div class="input-group position-relative">
+                                                {!! Form::text('last_name', null, ["class" => "search-input-text form-control", "data-column" => 1, "placeholder" => 'Nazwisko']) !!}
+                                                <span class="form-clear d-none reset-data"><i class="material-icons">clear</i></span>
+                                            </div>
+                                        </th>
+                                        <th>
+                                            <div class="input-group position-relative">
+                                                {!! Form::text('email', null, ["class" => "search-input-text form-control", "data-column" => 2, "placeholder" => 'email']) !!}
+                                                <span class="form-clear d-none reset-data"><i class="material-icons">clear</i></span>
+                                            </div>
+                                        </th>
+                                        <th>
+                                            <div class="input-group position-relative">
+                                                {!! Form::text('phone_nr', null, ["class" => "search-input-text form-control", "data-column" => 3, "placeholder" => 'Nr telefonu']) !!}
+                                                <span class="form-clear d-none reset-data"><i class="material-icons">clear</i></span>
+                                            </div>
+                                        </th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div><!--table-responsive-->
+                    </div><!-- /.box-body -->
+                </div><!--box-->
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('after-scripts')
+    {{-- For DataTables --}}
+    <script>
+
+        (function () {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            var dataTable = $('#clients-table').dataTable({
+                processing: true,
+                serverSide: true,
+                ajax: {
+                    url: '{{ route("admin.client.get") }}',
+                    type: 'post',
+                    data: {status: false, trashed: true}
+                },
+                columns: [
+                    {data: 'first_name', name: 'clients.first_name'},
+                    {data: 'last_name', name: 'clients.last_name'},
+                    {data: 'email', name: 'clients.email'},
+                    {data: 'phone_nr', name: 'clients.phone_nr', sortable: false},                
+                    {data: 'status', className: 'text-center', name: 'clients.status', render: function ( data, type, row, meta ) {
+                        if(data=1) {
+                            return '<span class="badge badge-success">Tak</a>';
+                        } else {
+                            return '<span class="badge badge-danger">Nie</a>';
+                        }
+                    }},
+                    {data: 'services', name: '{{config('service.services_table')}}.name', sortable: false},
+                    {data: 'created_at', name: 'clients.created_at'},
+                    {data: 'updated_at', name: 'clients.updated_at'},
+                    {data: 'actions', name: 'actions', className: 'text-center', searchable: false, sortable: false}
+                ],
+                order: [[0, "asc"]],
+                searchDelay: 500,
+                dom: "<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'f>><'row'<'col-sm-12'tr>><'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+                buttons: {
+                    buttons: [
+                        { extend: 'copy', className: 'copyButton',  exportOptions: {columns: [ 0, 1, 2, 3, 4, 5, 6 ]  }},
+                        { extend: 'csv', className: 'csvButton',  exportOptions: {columns: [ 0, 1, 2, 3, 4, 5, 6 ]  }},
+                        { extend: 'excel', className: 'excelButton',  exportOptions: {columns: [ 0, 1, 2, 3, 4, 5, 6 ]  }},
+                        { extend: 'pdf', className: 'pdfButton',  exportOptions: {columns: [ 0, 1, 2, 3, 4, 5, 6 ]  }},
+                        { extend: 'print', className: 'printButton',  exportOptions: {columns: [ 0, 1, 2, 3, 4, 5, 6 ]  }}
+                    ]
+                },
+                language: {
+                    @lang('datatable.strings')
+                }
+            });
+
+            Backend.DataTableSearch.init(dataTable);
+            bootstrapClearButton();
+        })();
+       
+    </script>
+@endsection
