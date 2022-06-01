7ca0a05429bd687eb21009964761a6792b355590
NLS667 <kosu@uroczysko.org>
2022-06-01 13:42:47 +0200

    commit

--- app/Repositories/Backend/Client/ClientRepository.php
+++ app/Repositories/Backend/Client/ClientRepository.php
@@ -63,7 +63,7 @@ class ClientRepository extends BaseRepository
                 config('clients.clients_table').'.created_at',
                 config('clients.clients_table').'.updated_at',
                 config('clients.clients_table').'.deleted_at',
-                DB::raw('GROUP_CONCAT(service_categories.name) as services'),
+                DB::raw('GROUP_CONCAT(service_categories.name SEPARATOR ', ') as services'),
             ])
             ->groupBy('clients.id');
 
