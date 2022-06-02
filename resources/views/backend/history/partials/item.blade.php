3c0e0df12e329a330ec5360c1abab8f8e6d60778
NLS667 <kosu@uroczysko.org>
2022-06-02 20:43:08 +0200

    commit

--- resources/views/backend/history/partials/item.blade.php
+++ resources/views/backend/history/partials/item.blade.php
@@ -4,10 +4,7 @@
 	</div>
     <div class="timeline-panel">
     	<div class="timeline-heading">
-    		<span class="badge badge-pill badge-{{$historyItem->class}}">{{ $historyItem->created_at->diffForHumans() }}</span>
-    	</div>
-    	<div class="timeline-body">
-    		<strong>{{ $historyItem->user->name }}</strong> {!! history()->renderDescription($historyItem->text, $historyItem->assets) !!}
+    		<span class="badge badge-pill badge-{{$historyItem->class}}">{{ $historyItem->created_at->diffForHumans() }}</span>&nbsp;<strong>{{ $historyItem->user->name }}</strong> {!! history()->renderDescription($historyItem->text, $historyItem->assets) !!}
     	</div>
     </div><!--timeline-item-->
 </li>
