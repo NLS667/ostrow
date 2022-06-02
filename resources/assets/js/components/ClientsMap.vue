1cfd37d6fcae00b96fd4652a7fa954442bbdb990
NLS667 <kosu@uroczysko.org>
2022-06-02 21:54:44 +0200

    commit

--- resources/assets/js/components/ClientsMap.vue
+++ resources/assets/js/components/ClientsMap.vue
@@ -25,7 +25,6 @@
         	if ($('#leaflet-map').length) {
         		lf.initMap();
             	lf.initMarkers();
-                lf.getAllClients();
 			}
         },
         methods: {
@@ -54,25 +53,12 @@
                 })
 
                 this.pins.forEach((pin) => {
-                    pin.leafletObject = L.marker(pin.coords).bindPopup(pin.content);
+                    pin.leafletObject = L.marker(pin.coords, {icon: pin.icon}).bindPopup(pin.content);
                     pin.leafletObject.addTo(this.clients_map);
                     if(this.data.mapMode == 'small'){
                         this.centerLeafletMapOnMarker(this.clients_map, pin.leafletObject)
                     } 
                 })        
-            },
-            getAllClients() {
-                $.ajax({
-                  type:'get',
-                  async:false,
-                  dataType: "JSON",
-                  //data: {alarm_id: this.data.vehicles},
-                  url:'admin/client/getclientslocations',
-                  success: function(result){
-                      //$('h3.counter').text(result.players);
-                      this.log('testowo:'+result);
-                  }
-                });
             },
             centerLeafletMapOnMarker(map, marker){
                 var latLngs = [ marker.getLatLng() ];
