058a60c43753d47e1b7f394701ef4ee7c945f9bd
NLS667 <kosu@uroczysko.org>
2022-06-03 14:52:49 +0200

    commit

--- resources/views/backend/client/show.blade.php
+++ resources/views/backend/client/show.blade.php
@@ -14,6 +14,7 @@
 	            	<div class="card-body ">
 	            		<div class="row">
 	            			<div class="col-md-4">
+	            				<h4 class="card-title">Adres</h>
 	            				<p class="card-text">
 		                			{{$client->adr_street}}&nbsp;{{$client->adr_street_nr}}<br>
 		                			{{$client->adr_zipcode}}&nbsp;{{$client->adr_city}}<br>
@@ -22,7 +23,9 @@
 		                		</p>
 	            			</div>
 							<div class="col-md-4">
-								<a href="mailto:{{$client->email}}"><strong>{{$client->email}}</strong></a><br>
+								<h4 class="card-title">Adres</h><br>
+								<a href="mailto:{{$client->email}}"><strong>{{$client->email}}</strong></a><br><br>
+								<h4 class="card-title">Telefon</h><br>
 		                		<strong>{{$client->phone_nr}}</strong>
 							</div>
 	            			
