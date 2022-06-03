80482bdd87cb5b70ab6365349295439535e33785
NLS667 <kosu@uroczysko.org>
2022-06-03 16:18:17 +0200

    commit

--- app/Http/Controllers/Backend/Client/ClientController.php
+++ app/Http/Controllers/Backend/Client/ClientController.php
@@ -101,7 +101,7 @@ class ClientController extends Controller
      */
     public function show(Client $client, ShowClientRequest $request)
     {
-        $services = Service::where('client_id', $client->id)->all();
+        $services = Service::where('client_id', $client->id)->get();
         \Log::info(json_encode($services));
         $serviceCatIds = [];
         foreach($services as $service){
