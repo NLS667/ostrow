6e675111549ed2c26e66896437f86a633346e4a4
NLS667 <kosu@uroczysko.org>
2022-06-03 02:40:33 +0200

    commit

--- resources/assets/js/components/ClientsMap.vue
+++ resources/assets/js/components/ClientsMap.vue
@@ -84,15 +84,17 @@
                 })
                 console.log(this.layerMarkers);
                 var first = true;
-                this.layerMarkers.forEach((lm) => {                    
+                this.layerMarkers.forEach((lm) => {
+                    var allGroups = new L.LayerGroup();          
                     var layerGroup = L.layerGroup(lm.markers);
+                    allGroups.addLayer(layerGroup);
                     if(first){
                         layerGroup.addTo(this.clients_map);
                         first = false;
                     }                    
                     this.layerControl.addOverlay(layerGroup, lm.name);
                 })
-                this.clients_map.addControl( new L.Control.Search({layer: this.layerGroup}) );
+                this.clients_map.addControl( new L.Control.Search({layer: allGroups}) );
 
                 //this.pins.forEach((pin) => {
                 //    pin.leafletObject = L.marker(pin.coords).bindPopup(pin.content);
