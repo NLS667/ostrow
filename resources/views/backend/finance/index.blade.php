e42fa8a573e6e71092e3ee45f72b4bd430394134
NLS667 <kosu@uroczysko.org>
2022-06-07 19:59:40 +0200

    commit

--- resources/views/backend/finance/index.blade.php
+++ resources/views/backend/finance/index.blade.php
@@ -88,9 +88,9 @@
                     '<td class="text-right '+style+'">'+service.left_amount+'</td>'+
                     '<td></td>'+
                 '</tr>';
-                amountTotal += Number(service.deal_amount);
-                advanceTotal += Number(service.deal_advance);
-                leftTotal += Number(service.left_amount);
+                amountTotal += +service.deal_amount;
+                advanceTotal += +service.deal_advance;
+                leftTotal += +service.left_amount;
               }
               html += '<tr>'+
                     '<td></td>'+
