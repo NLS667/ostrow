c6f809cf67f0e0cb8256ee827f248b3bcb95aad8
NLS667 <kosu@uroczysko.org>
2022-06-07 18:09:04 +0200

    commit

--- resources/views/backend/finance/index.blade.php
+++ resources/views/backend/finance/index.blade.php
@@ -127,6 +127,27 @@
                 }
             });
 
+            function displayServices(services) {
+  
+              var html = '';
+              
+              // i=1 - Skip the first house, its in the DT row.
+              for (i=1; i<services.length; i++) {
+                var service = services[i];
+                
+                html += '<tr>'+
+                    '<td></td>'+
+                    '<td></td>'+
+                    '<td>'+service.short_name+'</td>'+
+                    '<td>'+service.deal_amount+'</td>'+
+                    '<td>'+service.deal_advance+'</td>'+
+                    '<td></td>'+
+                '</tr>';
+              }
+              
+              return $(html).toArray();
+            }
+
             var dataTable = $('#finance-table').dataTable({
                 columnDefs: [
                     {"className": "dt-center", "targets": "_all"}
@@ -173,6 +194,18 @@
                     {data: 'left_amount', name: 'clients.left', className: 'text-right', searchable: false, sortable: false}, 
                     {data: 'actions', name: 'actions', className: 'text-center', searchable: false, sortable: false}
                 ],
+                initComplete: function (settings, json) {
+                   
+                    var api = this.api();
+                      
+                    api.rows().every( function ( rowIdx, tableLoop, rowLoop ) {
+                        var data = this.data();
+                        
+                        if (data.services) {
+                            this.child( displayServices(data.services) ).show();
+                        }
+                    });
+                },
                 order: [[0, "asc"]],
                 searchDelay: 500,
                 dom: "<'row'<'col-sm-12 col-md-2'l><'col-sm-12 col-md-8'B><'col-sm-12 col-md-2'f>><'row'<'col-sm-12'tr>><'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
