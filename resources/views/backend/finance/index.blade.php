9e63e284477495562d8d7e2589eee0b153805fa5
NLS667 <kosu@uroczysko.org>
2022-06-07 16:45:53 +0200

    commit

--- resources/views/backend/finance/index.blade.php
+++ resources/views/backend/finance/index.blade.php
@@ -144,9 +144,9 @@
                     {data: 'first_name', name: 'clients.first_name'},
                     {data: 'adr_street', name: 'clients.adr_street'}, 
                     {data: 'service', name: 'service_categories.short_name', sortable: false}, 
-                    {data: 'deal_amount', name: 'service.deal_amount'},              
-                    {data: 'deal_advance', name: 'service.deal_advance'},
-                    {data: 'left_amount', name: 'clients.left'}, 
+                    {data: 'deal_amount', name: 'service.deal_amount', className: 'text-right'},              
+                    {data: 'deal_advance', name: 'service.deal_advance', className: 'text-right'},
+                    {data: 'left_amount', name: 'clients.left', className: 'text-right'}, 
                     {data: 'actions', name: 'actions', className: 'text-center', searchable: false, sortable: false}
                 ],
                 order: [[0, "asc"]],
