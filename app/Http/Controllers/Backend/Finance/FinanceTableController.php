264f18ea8ec4f2dfa3d7ad039baff4c44f566516
NLS667 <kosu@uroczysko.org>
2022-06-07 16:03:21 +0200

    commit

--- app/Http/Controllers/Backend/Finance/FinanceTableController.php
+++ app/Http/Controllers/Backend/Finance/FinanceTableController.php
@@ -72,7 +72,7 @@ class FinanceTableController extends Controller
                 config('clients.clients_table').'.adr_home_nr',
                 config('clients.clients_table').'.adr_zipcode',
                 config('clients.clients_table').'.adr_city',
-                DB::raw('GROUP_CONCAT(service_categories.short_name SEPARATOR " | ") as services'),
+                'service_categories.short_name',
                 'services.offered_at',
                 'services.signed_at',
                 'services.installed_at',
