8f4e33c167d1b7fd7cafd437ae20cfb8e2382e00
NLS667 <kosu@uroczysko.org>
2022-06-07 19:12:46 +0200

    commit

--- resources/views/backend/finance/index.blade.php
+++ resources/views/backend/finance/index.blade.php
@@ -68,7 +68,7 @@
               // i=1 - Skip the first house, its in the DT row.
               for (i=1; i<services.length; i++) {
                 var service = services[i];
-                if(Number(service.left_amount) > 0){
+                if(Number(service.left_amount) < 0){
                     var style = 'style="color: red;"';
                 } else 
                 {
