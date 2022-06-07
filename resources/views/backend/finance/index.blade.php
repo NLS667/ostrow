77d773ea8f927f8a250360befec842b543fe97be
NLS667 <kosu@uroczysko.org>
2022-06-07 19:27:43 +0200

    commit

--- resources/views/backend/finance/index.blade.php
+++ resources/views/backend/finance/index.blade.php
@@ -144,10 +144,10 @@
                     {data: 'actions', name: 'actions', className: 'text-center', searchable: false, sortable: false}
                 ],
                 createdRow: function (row, data, index) {
-                    if (data[5].replace(/[\$,]/g, '') * 1 > 0) {
-                        $('td', row).eq(5).addClass('text-danger');
-                    } else {
+                    if (data[5] == '0.00') {
                         $('td', row).eq(5).addClass('text-success');
+                    } else {
+                        $('td', row).eq(5).addClass('text-danger');
                     }
                 },
                 initComplete: function (settings, json) {
