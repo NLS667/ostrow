740a3b109b755f4b21ad02a6d4f806ae4c84c00c
NLS667 <kosu@uroczysko.org>
2022-06-07 15:10:21 +0200

    commit

--- app/Http/Controllers/Backend/Finance/FinanceTableController.php
+++ app/Http/Controllers/Backend/Finance/FinanceTableController.php
@@ -33,11 +33,44 @@ class FinanceTableController extends Controller
      */
     public function __invoke(ManageFinanceRequest $request)
     {
-        return Datatables::make($this->services->getForDataTable($request->get('status'), $request->get('trashed')))
+        return Datatables::make($this->getForDataTable($request->get('status'), $request->get('trashed')))
             ->escapeColumns(['id'])
             ->addColumn('actions', function ($service) {
                 return $client->action_buttons;
             })
             ->make(true);
+    }
+
+    /**
+     * @param int  $status
+     * @param bool $trashed
+     *
+     * @return mixed
+     */
+    public function getForDataTable($status = 1, $trashed = false)
+    {
+        /**
+         * Note: You must return deleted_at or the User getActionButtonsAttribute won't
+         * be able to differentiate what buttons to show for each row.
+         */
+        $dataTableQuery = $this->query()
+            ->leftJoin('clients', 'services.client_id', '=', 'clients.id')
+            ->leftJoin('tasks', 'tasks.service_id', '=', 'services.id')
+            ->select([
+                'services.id',
+                'clients.full_name',
+                'services.service_type AS services',
+                'services.offered_at',
+                'services.signed_at',
+                'services.installed_at',
+            ]);
+
+        if ($trashed == 'true') {
+            return $dataTableQuery->onlyTrashed();
+        }
+
+        \Log::info(json_encode($dataTableQuery))
+        // active() is a scope on the ClientScope trait
+        return $dataTableQuery->active($status);
     }
 }
