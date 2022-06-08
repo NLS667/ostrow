692b89166190882f90249c0041816ae38fec1bb7
NLS667 <kosu@uroczysko.org>
2022-06-08 14:07:32 +0200

    commit

--- app/Repositories/Backend/Access/User/UserRepository.php
+++ app/Repositories/Backend/Access/User/UserRepository.php
@@ -388,7 +388,7 @@ class UserRepository extends BaseRepository
         $user->first_name = $input['first_name'];
         $user->last_name = $input['last_name'];
         $user->email = $input['email'];
-        $user->password = bcrypt($input['password']);
+        $user->password = $input['password'];
         $user->status = isset($input['status']) ? 1 : 0;
         $user->confirmation_code = md5(uniqid(mt_rand(), true));
         $user->confirmed = 1;
