7f98060e25a64ee46fa88fd11f3aa29844632ecf
NLS667 <kosu@uroczysko.org>
2022-06-08 23:45:18 +0200

    commit

--- resources/views/backend/client/show.blade.php
+++ resources/views/backend/client/show.blade.php
@@ -73,6 +73,11 @@
 	            		</div>
             		</div>
             	</div>
+            	<div class="card">
+            		<div class="card-body">
+            			<textarea name="content" class="form-control my-editor">{!! old('content', $content) !!}</textarea>
+            		</div>
+            	</div>
         	</div>
         	<div class="col-md-4">
         		<div class="card ">
@@ -84,4 +89,46 @@
     	</div>    		
   	</div>
 </div>
+@endsection
+
+@section('after-scripts')
+	<script>
+  var editor_config = {
+    path_absolute : "/",
+    selector: 'textarea.my-editor',
+    relative_urls: false,
+    plugins: [
+      "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+      "searchreplace wordcount visualblocks visualchars code fullscreen",
+      "insertdatetime media nonbreaking save table directionality",
+      "emoticons template paste textpattern"
+    ],
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+    file_picker_callback : function(callback, value, meta) {
+      var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+      var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+      var cmsURL = editor_config.path_absolute + 'laravel-filemanager?editor=' + meta.fieldname;
+      if (meta.filetype == 'image') {
+        cmsURL = cmsURL + "&type=Images";
+      } else {
+        cmsURL = cmsURL + "&type=Files";
+      }
+
+      tinyMCE.activeEditor.windowManager.openUrl({
+        url : cmsURL,
+        title : 'Filemanager',
+        width : x * 0.8,
+        height : y * 0.8,
+        resizable : "yes",
+        close_previous : "no",
+        onMessage: (api, message) => {
+          callback(message.content);
+        }
+      });
+    }
+  };
+
+  tinymce.init(editor_config);
+</script>
 @endsection
