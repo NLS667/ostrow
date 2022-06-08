4f11186b030ec5665c3d76c1293a93afa04e9864
NLS667 <kosu@uroczysko.org>
2022-06-08 16:00:28 +0200

    commit

--- resources/views/backend/task/index.blade.php
+++ resources/views/backend/task/index.blade.php
@@ -110,7 +110,7 @@
                     {"className": "dt-center", "targets": "_all"}
                 ],
                 processing: true,
-                serverSide: true,
+                serverSide: false,
                 searching: true,
 
                 ajax: {
