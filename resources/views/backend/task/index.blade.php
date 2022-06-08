9ff334d9b59eac1e1d29015ab5f2cb0912602c9a
NLS667 <kosu@uroczysko.org>
2022-06-08 17:11:22 +0200

    commit

--- resources/views/backend/task/index.blade.php
+++ resources/views/backend/task/index.blade.php
@@ -166,7 +166,7 @@
                     @lang('datatable.strings')
                 }
             });
-            var q_search = "{{ $q_search }}";
+            var q_search = "{{ $q_status }}";
             if(q_search){
                 console.log(q_search);
                 dataTable.columns( '.status' ).search( q_search ).draw();
