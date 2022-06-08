ada2edd07e5c090a5f45fae0582a5dcd8f2c3972
NLS667 <kosu@uroczysko.org>
2022-06-08 12:18:20 +0200

    commit

--- resources/views/backend/access/users/deactivated.blade.php
+++ resources/views/backend/access/users/deactivated.blade.php
@@ -108,7 +108,7 @@
                     {data: 'roles', name: '{{config('access.roles_table')}}.name', sortable: false},
                     {data: 'created_at', name: '{{config('access.users_table')}}.created_at'},
                     {data: 'updated_at', name: '{{config('access.users_table')}}.updated_at'},
-                    {data: 'actions', name: 'action', className: 'text-right td-actions', searchable: false, sortable: false}
+                    {data: 'actions', name: 'action', className: 'text-center td-actions', searchable: false, sortable: false}
                 ],
                 order: [[0, "asc"]],
                 searchDelay: 500,
