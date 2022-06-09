807e2efcb24a6582d78e79e4878d1072307a1cba
NLS667 <kosu@uroczysko.org>
2022-06-09 12:48:16 +0200

    commit

--- app/Handlers/LfmConfigHandler.php
+++ app/Handlers/LfmConfigHandler.php
@@ -16,6 +16,7 @@ class LfmConfigHandler extends \UniSharp\LaravelFilemanager\Handlers\ConfigHandl
             $refererURL = request()->headers->get('referer');
             \Log::info(json_encode($refererURL));
             $lastSegment = basename(parse_url($refererURL, PHP_URL_PATH));
+            \Log::info(json_encode($lastSegment));
             if(is_numeric($lastSegment)){
                 $client = Client::where('id', $lastSegment)->first();
                 return $client->name;
