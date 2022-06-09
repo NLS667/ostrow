7746fc4b7f6d0f0b609ea97d3dfccf3f23317c81
NLS667 <kosu@uroczysko.org>
2022-06-09 12:25:02 +0200

    commit

--- app/Handlers/LfmConfigHandler.php
+++ app/Handlers/LfmConfigHandler.php
@@ -4,12 +4,13 @@ namespace App\Handlers;
 
 use Illuminate\Support\Facades\URL;
 use App\Models\Client\Client;
+use App\Http\Requests\Request;
 
 class LfmConfigHandler extends \UniSharp\LaravelFilemanager\Handlers\ConfigHandler
 {
     public function userField()
     {
-        $currentURL = URL::current();
+        $currentURL = Request::server('HTTP_REFERER').
         $lastSegment = basename(parse_url($currentURL, PHP_URL_PATH));
         $client = Client::where('id', $lastSegment)->first();
         return $client->name;
