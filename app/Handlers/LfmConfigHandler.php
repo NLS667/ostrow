b4c05919b02f03d9e1a61f23c71db3214d56ae20
NLS667 <kosu@uroczysko.org>
2022-06-09 12:40:25 +0200

    commit

--- app/Handlers/LfmConfigHandler.php
+++ app/Handlers/LfmConfigHandler.php
@@ -10,13 +10,14 @@ class LfmConfigHandler extends \UniSharp\LaravelFilemanager\Handlers\ConfigHandl
 {
     public function userField()
     {
-        $currentURL = request()->headers->get('referer');
-
-        \Log::info(json_encode($currentURL));
-        $lastSegment = basename(parse_url($currentURL, PHP_URL_PATH));
-        if(is_numeric($lastSegment)){
-            $client = Client::where('id', $lastSegment)->first();
-            return $client->name;
+        if($request->is('*/laravel-filemanager'){
+            $currentURL = request()->headers->get('referer');
+            \Log::info(json_encode($currentURL));
+            $lastSegment = basename(parse_url($currentURL, PHP_URL_PATH));
+            if(is_numeric($lastSegment)){
+                $client = Client::where('id', $lastSegment)->first();
+                return $client->name;
+            }
         }
     }
 }
