6b6dfb80f4589e61f65f265545e2da621785d09f
NLS667 <kosu@uroczysko.org>
2022-06-13 12:32:06 +0200

    commit

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -40,7 +40,7 @@
         </div>
         <div class="col-lg-3 col-md-6 col-sm-6">
           <div class="card card-stats">
-            <div class="card-header card-header-danger card-header-icon">
+            <div class="card-header card-header-warning card-header-icon">
               <div class="card-icon">
                 <i class="material-icons">alarm</i>
               </div>
@@ -57,23 +57,22 @@
         </div>
         <div class="col-lg-3 col-md-6 col-sm-6">
           <div class="card card-stats">
-            <div class="card-header card-header-warning card-header-icon">
+            <div class="card-header card-header-danger card-header-icon">
               <div class="card-icon">
                 <i class="material-icons">content_copy</i>
               </div>
-              <p class="card-category">Zajęte miejsce</p>
-              <h3 class="card-title">49/50
-                <small>GB</small>
-              </h3>
+              <p class="card-category">Zadania po terminie</p>
+              <h3 class="card-title">{{ $data['overdueTaskCount'] }}</h3>
             </div>
             <div class="card-footer">
               <div class="stats">
-                <i class="material-icons text-danger">warning</i> Zaraz zabraknie miejsca...
+                <i class="material-icons">history</i> Przed chwilą
               </div>
             </div>
           </div>
         </div>
       </div>
+      @permission('view-map')
       <div class="row">
         <div class="col-md-12">
           <div class="card ">
@@ -88,6 +87,7 @@
           </div>
         </div>
       </div>
+      @endauth
     </div>
   </div>
 @endsection
