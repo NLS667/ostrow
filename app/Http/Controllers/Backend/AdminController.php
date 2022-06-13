0bc8ff6f1089bad8d23c386c8e2772162b9558e6
NLS667 <kosu@uroczysko.org>
2022-06-13 12:42:29 +0200

    commit

--- app/Http/Controllers/Backend/AdminController.php
+++ app/Http/Controllers/Backend/AdminController.php
@@ -75,7 +75,13 @@ class AdminController extends Controller
                 }
             }  
         }
-        return view('backend.index')->with('map_data', $map_data)->with('data', $data);
+        if(auth()->user->hasRole('Pracownik'))
+        {
+            return view('backend.index')->with('data', $data);
+        } else {
+            return view('backend.index')->with('map_data', $map_data)->with('data', $data);
+        }
+        
     }
 
     /**
