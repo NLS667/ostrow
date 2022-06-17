c376e0fada2aa15e5625a5329f9181727be81dde
NLS667 <kosu@uroczysko.org>
2022-06-17 12:37:04 +0200

    commit

--- database/seeders/Access/PermissionTableSeeder.php
+++ database/seeders/Access/PermissionTableSeeder.php
@@ -762,6 +762,20 @@ class PermissionTableSeeder extends Seeder
         $permModel->updated_at = null;
         $permModel->save();
 
+        /**
+         * Dictionaries.
+         */
+        $permission_model = config('access.permission');
+        $permModel = new $permission_model();
+        $permModel->name = 'view-dict-management';
+        $permModel->display_name = 'Dostęp do Słowników';
+        $permModel->sort = 59;
+        $permModel->created_by = 1;
+        $permModel->updated_by = null;
+        $permModel->created_at = Carbon::now();
+        $permModel->updated_at = null;
+        $permModel->save();
+
         $this->enableForeignKeys();
     }
 }
