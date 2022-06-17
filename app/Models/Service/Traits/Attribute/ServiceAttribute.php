d0af25a0bc2c6fa536d35e3701b2f8d0991e48f8
NLS667 <kosu@uroczysko.org>
2022-06-17 13:32:00 +0200

    commit

--- app/Models/Service/Traits/Attribute/ServiceAttribute.php
+++ app/Models/Service/Traits/Attribute/ServiceAttribute.php
@@ -41,6 +41,24 @@ trait ServiceAttribute
     }
 
     /**
+     * Get logged in user permission related to user management grid.
+     *
+     * @return array
+     */
+    public function getUserPermission()
+    {
+        $userPermission = [];
+        $attributePermission = ['34', '35'];
+        foreach (access()->user()->permissions as $permission) {
+            if (in_array($permission->id, $attributePermission)) {
+                $userPermission[] = $permission->name;
+            }
+        }
+
+        return $userPermission;
+    }
+
+    /**
      * @return string
      */
     
