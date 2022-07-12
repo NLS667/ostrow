d56764605956413f3ccd17d1e511f5b95c0f0655
NLS667 <kosu@uroczysko.org>
2022-07-12 14:47:11 +0200

    commit

--- app/Repositories/Backend/Client/ClientRepository.php
+++ app/Repositories/Backend/Client/ClientRepository.php
@@ -132,11 +132,11 @@ class ClientRepository extends BaseRepository
         $tasks = $request->get('tasks');
         $emails = $request->get('emails');
         $phones = $request->get('phones');
-        \Log::info($emails);
+        \Log::info(json_encode($data));
         $data->emails = json_encode($emails);
         $data->phones = json_encode($phones);
         \Log::info($data->emails);
-        
+
         DB::transaction(function () use ($client, $data, $services, $tasks) {
             if ($client->update($data)) {
                 $client->status = isset($data['status']) && $data['status'] == '1' ? 1 : 0;
