462f9efadf4b576e63c88605605f73fc5d20182a
NLS667 <kosu@uroczysko.org>
2022-07-12 18:49:24 +0200

    commit

--- resources/views/backend/service/create.blade.php
+++ resources/views/backend/service/create.blade.php
@@ -17,7 +17,7 @@
                 <h4 class="card-title">Dodaj Usługę</h4>
               </div>
             </div>
-            <div class="card-body ">              
+            <div class="card-body ">    
               <div class="row">
                 {{-- Client --}}
                 <div class="col-sm-12 form-group bmd-form-group {{ $errors->has('client_id') ? ' has-danger' : '' }}">
@@ -48,25 +48,15 @@
                   <p>Brak dostępnych Kategorii Usług. {{ link_to_route('admin.serviceCategory.index', 'Dodaj ') }}nową Kategorię Usług</p>
                   @endif
                 </div>
-              </div>
+              </div>              
               <div class="row">
-                {{-- Model --}}
-                <div class="col-sm-12 form-group bmd-form-group {{ $errors->has('model_id') ? ' has-danger' : '' }}">
-                  @if ($models->count())
-                  <select name="model_id" class="form-control select2 model-select" data-placeholder="Wybierz Model Urządzenia">
-                    <option></option>
-                    @foreach ($models as $model)
-                    <option value="{{$model->id}}" {{ old('model_id') == $model->id ? "selected":"" }}>{{ $model->producer->name.' '.$model->name}}</option>
-                    @endforeach                  
-                  </select>
-                  @else
-                  <p>Brak dostępnych Modeli. {{ link_to_route('admin.model.index', 'Dodaj ') }}nowy Model</p>
-                  @endif
+                <div class="col-md-12">
+                  <h4 class="title">Informacje o usłudze</h4>
                 </div>
               </div>
               <div class="row">
                 {{-- Offer Date --}}
-                <div class="col-sm-12 form-group bmd-form-group {{ $errors->has('offered_at') ? ' has-danger' : '' }}">
+                <div class="col-sm-4 form-group bmd-form-group {{ $errors->has('offered_at') ? ' has-danger' : '' }}">
                   <label class="bmd-label-floating">Data Oferty</label>
                   <input class="form-control datepicker" name="offered_at" id="input-offered_at" type="text" value="{{ old('offered_at') }}" />
                   @if ($errors->has('offered_at'))
@@ -74,10 +64,8 @@
                   <span id="offered_at-error" class="error text-danger" for="input-offered_at">{{ $errors->first('offered_at') }}</span>
                   @endif
                 </div>
-              </div>
-              <div class="row">
                 {{-- Deal Date --}}
-                <div class="col-sm-12 form-group bmd-form-group {{ $errors->has('signed_at') ? ' has-danger' : '' }}">
+                <div class="col-sm-4 form-group bmd-form-group {{ $errors->has('signed_at') ? ' has-danger' : '' }}">
                   <label class="bmd-label-floating">Data Umowy</label>
                   <input class="form-control datepicker" name="signed_at" id="input-signed_at" type="text" value="{{ old('signed_at') }}" />
                   @if ($errors->has('signed_at'))
@@ -85,10 +73,8 @@
                   <span id="signed_at-error" class="error text-danger" for="input-signed_at">{{ $errors->first('signed_at') }}</span>
                   @endif
                 </div>
-              </div>
-              <div class="row">
                 {{-- Montage Date --}}
-                <div class="col-sm-12 form-group bmd-form-group {{ $errors->has('installed_at') ? ' has-danger' : '' }}">
+                <div class="col-sm-4 form-group bmd-form-group {{ $errors->has('installed_at') ? ' has-danger' : '' }}">
                   <label class="bmd-label-floating">Data Montażu</label>
                   <input class="form-control datepicker" name="installed_at" id="input-installed_at" type="text" value="{{ old('installed_at') }}" />
                   @if ($errors->has('installed_at'))
@@ -108,17 +94,39 @@
                   @endif
                 </div>
               </div>
-              <div class="row">
-                {{-- Deal Advance --}}
-                <div class="col-sm-12 form-group bmd-form-group {{ $errors->has('deal_advance') ? ' has-danger' : '' }}">
-                  <label class="bmd-label-floating">Wpłacona zaliczka</label>
-                  <input class="form-control{{ $errors->has('deal_advance') ? ' is-invalid' : '' }}" name="deal_advance" id="input-deal_advance" type="text" value="{{ old('deal_advance') }}" />
-                  @if ($errors->has('deal_advance'))
-                  <span class="material-icons form-control-feedback">clear</span>
-                  <span id="deal_advance-error" class="error text-danger" for="input-deal_advance">{{ $errors->first('deal_advance') }}</span>
-                  @endif
+              <div id="advance">
+                <div class="row">
+                  {{-- Deal Advance --}}
+                  <div class="col-sm-8 form-group bmd-form-group {{ $errors->has('deal_advance') ? ' has-danger' : '' }}">
+                    <label class="bmd-label-floating">Wpłacona zaliczka</label>
+                    <input class="form-control{{ $errors->has('deal_advance') ? ' is-invalid' : '' }}" name="deal_advance" id="input-deal_advance" type="text" value="{{ old('deal_advance') }}" />
+                    @if ($errors->has('deal_advance'))
+                    <span class="material-icons form-control-feedback">clear</span>
+                    <span id="deal_advance-error" class="error text-danger" for="input-deal_advance">{{ $errors->first('deal_advance') }}</span>
+                    @endif
+                  </div>
+                  <div class="col-sm-4">
+                    <button type="button" name="add_contact" id="add_contact" class="btn btn-primary">Dodaj zaliczkę</button>
+                  </div>
                 </div>
-              </div>           
+              </div>
+              <div id="devices">
+                <div class="row">
+                  {{-- Model --}}
+                  <div class="col-sm-12 form-group bmd-form-group {{ $errors->has('model_id') ? ' has-danger' : '' }}">
+                    @if ($models->count())
+                    <select name="model_id" class="form-control select2 model-select" data-placeholder="Wybierz Model Urządzenia">
+                      <option></option>
+                      @foreach ($models as $model)
+                      <option value="{{$model->id}}" {{ old('model_id') == $model->id ? "selected":"" }}>{{ $model->producer->name.' '.$model->name}}</option>
+                      @endforeach                  
+                    </select>
+                    @else
+                    <p>Brak dostępnych Modeli. {{ link_to_route('admin.model.index', 'Dodaj ') }}nowy Model</p>
+                    @endif
+                  </div>
+                </div>
+              </div>        
             </div>
             <div class="card-footer">
               {{ link_to_route('admin.service.index', 'Anuluj', [], ['class' => 'btn btn-danger btn-md']) }}
