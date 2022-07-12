594e57a57e74b8b17778eabc59f0cf624ce4abdc
NLS667 <kosu@uroczysko.org>
2022-07-12 22:24:12 +0200

    commit

--- app/Http/Responses/Backend/Client/ShowResponse.php
+++ app/Http/Responses/Backend/Client/ShowResponse.php
@@ -61,7 +61,7 @@ class ShowResponse implements Responsable
             $client_data[] = (object)[
                 'category' => $category->name.' ('.$category->short_name.')',
                 'service' => (object)['id' => $service->id, 'offered_at' => $service->offered_at, 'signed_at' => $service->signed_at, 'installed_at' => $service->installed_at],
-                'models' => $modelObj,
+                'models' => (object)$modelObj,
                 //'serial_number' => $model->serial_number,
                 //'producer' => $producer->name,
             ];
