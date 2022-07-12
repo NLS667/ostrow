19127314cdc01372e29f227fb662ab958ebd3819
NLS667 <kosu@uroczysko.org>
2022-07-12 22:28:12 +0200

    commit

--- app/Http/Responses/Backend/Client/ShowResponse.php
+++ app/Http/Responses/Backend/Client/ShowResponse.php
@@ -47,9 +47,9 @@ class ShowResponse implements Responsable
         foreach($this->serviceCategories as $category)
         {
             $service = Service::where('client_id', $this->client->id)->where('service_cat_id', $category->id)->first();
-            \Log::info($service->models);
             $models = json_decode($service->models);
             $modelsObj = [];
+            \Log::info(count($models));
             for($i=0;$i<count($models);$i++){
                 $model = Model::where('id', ($models[$i]))->first();
                 $modelsObj = (object)[
