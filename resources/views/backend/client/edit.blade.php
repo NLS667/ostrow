19462790618fcfbeb979f703d7354dd11666458c
NLS667 <kosu@uroczysko.org>
2022-07-12 11:43:51 +0200

    commit

--- resources/views/backend/client/edit.blade.php
+++ resources/views/backend/client/edit.blade.php
@@ -220,6 +220,11 @@
                 </div>
                 <div class="row">
                   <div class="col-md-12">
+                    <h4 class="title">Dane kontaktowe</h4>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-12">
                     <div class="row">
                       {{-- Contacts email --}}
                       <div class="col-sm-5 form-group bmd-form-group {{ $errors->has('emails') ? ' has-danger' : '' }}">
