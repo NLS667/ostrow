cd2211397fa644f65cb2850733e99dd50436f37f
NLS667 <kosu@uroczysko.org>
2022-07-13 22:53:08 +0200

    calendar additional features

--- resources/assets/js/components/Calendar.vue
+++ resources/assets/js/components/Calendar.vue
@@ -102,7 +102,7 @@ export default {
             }
           },
           eventClick: function (e) {
-            alert('Event: ' + info.event.title);
+            alert('Event: ' + e.event.title);
             this.current_task = e.event
             this.show_task_details_modal = true
           }
