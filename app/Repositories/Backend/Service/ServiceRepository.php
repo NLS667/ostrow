ff09572c22882ffcc0a17257138d2865fdf83f6c
NLS667 <kosu@uroczysko.org>
2022-07-18 18:54:22 +0200

    Poprawka formularza dodawania klienta

--- app/Repositories/Backend/Service/ServiceRepository.php
+++ app/Repositories/Backend/Service/ServiceRepository.php
@@ -78,6 +78,7 @@ class ServiceRepository extends BaseRepository
             $service->signed_at = $request['signed_at'];
             $service->installed_at = $request['installed_at'];
             $service->deal_amount = $request['deal_amount'];
+            $service->advance_date = json_encode($request['advance_date']);
             $service->deal_advance = json_encode($request['deal_advance']);
 
             $service->created_by = access()->user()->id;
