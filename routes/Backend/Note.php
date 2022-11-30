d71ac09c956f692e9e1746850fffa0bb218a9abd
NLS667 <kosu@uroczysko.org>
2022-11-30 12:54:03 +0100

    Notes system

--- routes/Backend/Note.php
+++ routes/Backend/Note.php
@@ -4,11 +4,15 @@
      * Producers Management
      */
     Route::group(['namespace' => 'Producer'], function () {
-            
+
+
             /*
              * For notes 
              */
             Route::post('note/add-note', [\App\Http\Controllers\Backend\Note\NoteController::class, 'store'])->name('note.add');
+            Route::post('note/edit', [\App\Http\Controllers\Backend\Note\NoteController::class, 'update'])->name('note.update');
+            Route::post('note/delete', [\App\Http\Controllers\Backend\Note\NoteController::class, 'destroy'])->name('note.destroy');
+            
 
             /*
              * Specific Note
