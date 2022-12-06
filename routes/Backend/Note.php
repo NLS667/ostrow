47d41a8e1a7723b3e3c29c52484ab7f2524ff9de
NLS667 <kosu@uroczysko.org>
2022-12-06 14:02:18 +0100

    Notes system

--- routes/Backend/Note.php
+++ routes/Backend/Note.php
@@ -10,8 +10,13 @@
              * For notes 
              */
             Route::post('note/add-note', [\App\Http\Controllers\Backend\Note\NoteController::class, 'store'])->name('note.add');
-            Route::post('note/edit', [\App\Http\Controllers\Backend\Note\NoteController::class, 'update'])->name('note.update');
-            Route::delete('note/delete/{note}', [\App\Http\Controllers\Backend\Note\NoteController::class, 'destroy'])->name('note.destroy');
+            //Route::post('note/edit', [\App\Http\Controllers\Backend\Note\NoteController::class, 'update'])->name('note.update');
+            //Route::delete('note/delete/{note}', [\App\Http\Controllers\Backend\Note\NoteController::class, 'destroy'])->name('note.destroy');
+
+            /*
+             * Note CRUD
+             */
+            Route::resource('note', NoteController::class);
             
 
             /*
