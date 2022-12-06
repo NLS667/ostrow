3a30851c70a0f479dfb7bbdea1ab67433fcc6816
NLS667 <kosu@uroczysko.org>
2022-12-06 15:11:53 +0100

    Notes system

--- resources/views/backend/client/show.blade.php
+++ resources/views/backend/client/show.blade.php
@@ -397,12 +397,12 @@
       let content = $('#input-update-content').val();
 
       const note_id = $(this).val();
-      var url = "{{ URL('/admin/note/:note_id')}}";
+      var url = "{{ url('/admin/note', [:note_id])}}";
       url = url.replace(':note_id',note_id);
       
       $.ajax({
         url: url,
-        type:"PATCH",
+        type:"PUT",
         data:{
           "_token": "{{ csrf_token() }}",
           content:content,
