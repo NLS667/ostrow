a8623cf4ad4debce96143360350b3d7251022d3e
NLS667 <kosu@uroczysko.org>
2022-12-06 14:21:30 +0100

    Notes system

---
+++ app/Http/Responses/Backend/Note/EditResponse.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Responses\Backend\Note;
+
+use Illuminate\Contracts\Support\Responsable;
+
+class EditResponse implements Responsable
+{
+    /**
+     * @var \App\Models\Note\Note
+     */
+    protected $note;
+
+
+    /**
+     * @param \App\Models\Note\Note $note
+     */
+    public function __construct($note)
+    {
+        $this->note = $note;
+    }
+
+    /**
+     * toReponse.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function toResponse($request)
+    {
+        return response()->json();
+    }
+}
