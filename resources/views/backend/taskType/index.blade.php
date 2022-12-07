7876188a7d227f924ed136565d1b68cf660e5ba7
NLS667 <kosu@uroczysko.org>
2022-12-07 15:21:46 +0100

    Task Types

---
+++ resources/views/backend/taskType/index.blade.php
@@ -0,0 +1,140 @@
+@extends('backend.layouts.app', ['activePage' => 'task-management', 'titlePage' => 'Zarządzanie Rodzajami Zadań'])
+
+@section('content')
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12"> 
+                <div class="card">
+                    <div class="card-header card-header-icon card-header-info d-flex justify-content-between align-items-center">
+                        <div class="col-md-6">
+                            <div class="card-icon">
+                                <i class="material-icons">handyman</i>
+                            </div>
+                            <h4 class="card-title">Rodzaje usług</h4>
+                        </div>
+                        <div class="card-tools">
+                            @include('backend.includes.partials.task-type-header-buttons')
+                        </div>
+                    </div><!-- /.card-header --> 
+
+                    <div class="card-body">
+                        <div class="table-responsive data-table-wrapper">
+                            <table id="task-type-table" class="table dataTable table-striped table-hover table-no-bordered dtr-inline" cellspacing="0" width="100%" style="width: 100%;">
+                                <thead>
+                                    <tr role="row">
+                                        <th>Nazwa</th>
+                                        <th>Opis</th>
+                                        <th>Zadań</th>
+                                        <th>Utworzona</th>
+                                        <th>Zmieniona</th>
+                                        <th>Akcje</th>
+                                    </tr>
+                                </thead>
+                                <tfoot>
+                                    <tr>
+                                        <th>Nazwa</th>
+                                        <th>Opis</th>
+                                        <th>Zadań</th>
+                                        <th>Utworzona</th>
+                                        <th>Zmieniona</th>
+                                        <th class="text-right">Akcje</th>
+                                    </tr>
+                                </tfoot>
+                                <thead class="transparent-bg">
+                                    <tr>
+                                        <th>
+                                            <div class="input-group position-relative">
+                                                {!! Form::text('name', null, ["class" => "search-input-text form-control", "data-column" => 0, "placeholder" => 'Nazwa']) !!}
+                                                <span class="form-clear d-none reset-data"><i class="material-icons">clear</i></span>
+                                            </div>
+                                        </th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div><!--table-responsive-->
+                    </div><!-- /.card-body -->
+                </div><!--card-->
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12"> 
+                <div class="card">
+                    <div class="card-header card-header-success d-flex justify-content-between align-items-center">
+                        <h4 class="card-title">Historia</h4>
+                        <div class="card-tools">
+                            <button class="btn btn-primary btn-round" data-toggle="collapse"  data-target="#collapseBody"><i class="fa fa-minus"></i></button>
+                        </div><!-- /.card tools -->
+                    </div><!-- /.card-header -->
+                    <div id="collapseBody" class="collapse">
+                        <div class="card-body">
+                        {!! history()->renderType('Rodzaje Zadań') !!}
+                        </div><!--card-body-->
+                    </div><!--collapse-->
+                </div><!--card box-info-->
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('after-scripts')
+    {{-- For DataTables --}}
+
+    <script>
+        $(function() {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+
+            var dataTable = $('#task-type-table').DataTable({
+                columnDefs: [
+                    {"className": "dt-center", "targets": "_all"}
+                ],
+                processing: true,
+                serverSide: false,
+                searching: true,
+
+                ajax: {
+                    url: '{{ route("admin.taskType.get") }}',
+                    type: 'post',
+                    data: {status: 1, trashed: false}
+                },
+                columns: [
+                    {data: 'name', name: 'task_types.name'},
+                    {data: 'description', name: 'task_types.description'},
+                    {data: 'tasks', name: 'tasks'},
+                    {data: 'created_at', name: 'task_types.created_at'},
+                    {data: 'updated_at', name: 'task_types.updated_at'},
+                    {data: 'actions', name: 'actions', className: 'text-center td-actions', searchable: false, sortable: false}
+                ],
+                order: [[3, "asc"]],
+                searchDelay: 500,
+                dom: "<'row'<'col-sm-12 col-md-2'l><'col-sm-12 col-md-8'B><'col-sm-12 col-md-2'f>><'row'<'col-sm-12'tr>><'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+                buttons: {
+                    buttons: [
+                        { extend: 'copyHtml5', className: 'copyButton d-none',  exportOptions: {columns: [ 0, 1, 2, 3, 4 ]  }},
+                        { extend: 'csvHtml5', className: 'csvButton d-none',  exportOptions: {columns: [ 0, 1, 2, 3, 4 ]  }},
+                        { extend: 'excelHtml5', className: 'excelButton d-none',  exportOptions: {columns: [ 0, 1, 2, 3, 4 ]  }},
+                        { extend: 'pdfHtml5', className: 'pdfButton d-none',  exportOptions: {columns: [ 0, 1, 2, 3, 4 ]  }},
+                        { extend: 'print', className: 'printButton d-none',  exportOptions: {columns: [ 0, 1, 2, 3, 4 ]  }}
+                    ]
+                },
+                language: {
+                    @lang('datatable.strings')
+                }
+            });
+
+            Backend.DataTableSearch.init(dataTable);
+            bootstrapClearButton();
+    
+        });
+    </script>
+@endsection
