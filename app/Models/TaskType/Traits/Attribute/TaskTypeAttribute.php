659dd3cc2b9f9856efd75f65d5e3fbd270b600b0
NLS667 <kosu@uroczysko.org>
2022-12-07 15:08:27 +0100

    Task Types

---
+++ app/Models/TaskType/Traits/Attribute/TaskTypeAttribute.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Models\TaskType\Traits\Attribute;
+
+/**
+ * Class TaskTypeAttribute.
+ */
+trait TaskTypeAttribute
+{
+    /**
+     * @return string
+     */
+    public function getEditButtonAttribute($class)
+    {
+        if (access()->allow('edit-tasktype')) {
+            return '<a class="'.$class.'" data-toggle="tooltip" data-placement="top" title="Edytuj" href="'.route('admin.taskType.edit', $this).'">
+            <span class="material-icons">edit</span>
+            </a>';
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getDeleteButtonAttribute($class)
+    {
+        if (access()->allow('delete-tasktype')) {
+            $name = ($class == '' || $class == 'dropdown-item') ? 'Usuń' : '';
+
+            return '<a class="'.$class.'" data-toggle="tooltip" data-placement="top" title="Usuń" href="'.route('admin.taskType.destroy', $this).'"
+            data-method="delete"
+            data-trans-button-cancel="Anuluj"
+            data-trans-button-confirm="Usuń"
+            data-trans-title="Czy na pewno?"><span class="material-icons">delete</span>'.$name.'</a>';
+        }
+
+        return '';
+    }
+
+    /**
+     * Get logged in user permission related to user management grid.
+     *
+     * @return array
+     */
+    public function getUserPermission()
+    {
+        $userPermission = [];
+        $attributePermission = ['68', '69'];
+        foreach (access()->user()->permissions as $permission) {
+            if (in_array($permission->id, $attributePermission)) {
+                $userPermission[] = $permission->name;
+            }
+        }
+
+        return $userPermission;
+    }
+
+    /**
+     * @return string
+     */
+    
+    public function getActionButtonsAttribute()
+    {
+        // Check if role have all permission
+        if (access()->user()->roles[0]->all) {
+            return $this->getEditButtonAttribute('btn btn-success btn-round').'
+            '.$this->getDeleteButtonAttribute('btn btn-danger btn-round');
+        } else {
+            $userPermission = $this->getUserPermission();
+            $permissionCounter = count($userPermission);
+            $actionButton = '';
+            $i = 1;
+
+            foreach ($userPermission as $value) {
+                $actionButton = $actionButton.' '.$this->getActionButtonsByPermissionName($value, $i);
+                $i++;
+            }
+            $actionButton .= '</ul></div>';
+
+            return $actionButton;
+        }
+    }
+
+    /**
+     * Get action button attribute by permission name.
+     *
+     * @param string $permissionName
+     * @param int    $counter
+     *
+     * @return string
+     */
+    public function getActionButtonsByPermissionName($permissionName, $counter)
+    {
+        switch ($permissionName) {
+            case 'edit-tasktype':
+            $button = $this->getEditButtonAttribute('btn btn-success btn-round');
+            break;
+            case 'delete-tasktype':
+            $button = $this->getDeleteButtonAttribute('btn btn-danger btn-round');
+            break;
+            default:
+            $button = '';
+            break;
+        }
+
+        return $button;
+    }
+    
+}
---
+++ app/Models/TaskType/Traits/Relationship/TaskTypeRelationship.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models\TaskType\Traits\Relationship;
+
+/**
+ * Class TaskTypeRelationship.
+ */
+trait TaskTypeRelationship
+{
+    /**
+     * @return mixed
+     */
+    public function tasks()
+    {
+        return;
+    }
+}
