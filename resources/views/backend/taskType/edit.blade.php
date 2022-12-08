11c2be2f9586a566bc213181f6d1ea2f05a013ce
NLS667 <kosu@uroczysko.org>
2022-12-08 10:38:43 +0100

    Task Types

--- resources/views/backend/taskType/edit.blade.php
+++ resources/views/backend/taskType/edit.blade.php
@@ -24,7 +24,7 @@
                   <label class="col-sm-2 col-form-label">Nazwa</label>
                   <div class="col-sm-7">
                     <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
-                      <input class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" id="input-name" type="text" placeholder="Nazwa" value="{{ old('name', $serviceCategory->name) }}" required="true" aria-required="true"/>
+                      <input class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" id="input-name" type="text" placeholder="Nazwa" value="{{ old('name', $taskType->name) }}" required="true" aria-required="true"/>
                       @if ($errors->has('name'))
                         <span id="name-error" class="error text-danger" for="input-name">{{ $errors->first('name') }}</span>
                       @endif
@@ -35,7 +35,7 @@
                   <label class="col-sm-2 col-form-label">Opis</label>
                   <div class="col-sm-7">
                     <div class="form-group{{ $errors->has('description') ? ' has-danger' : '' }}">
-                      <input class="form-control{{ $errors->has('description') ? ' is-invalid' : '' }}" name="description" id="input-description" type="text" placeholder="Opis" value="{{ old('description', $serviceCategory->description) }}" required="true" aria-required="true"/>
+                      <input class="form-control{{ $errors->has('description') ? ' is-invalid' : '' }}" name="description" id="input-description" type="text" placeholder="Opis" value="{{ old('description', $taskType->description) }}" required="true" aria-required="true"/>
                       @if ($errors->has('description'))
                         <span id="description-error" class="error text-danger" for="input-description">{{ $errors->first('description') }}</span>
                       @endif
