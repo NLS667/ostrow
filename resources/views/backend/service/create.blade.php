e15964c2dfc9f23268dc69bd6f817e94aa98b97a
NLS667 <kosu@uroczysko.org>
2022-12-13 14:46:23 +0100

    Service type logics

--- resources/views/backend/service/create.blade.php
+++ resources/views/backend/service/create.blade.php
@@ -173,12 +173,15 @@
                     </div>
                   </div>
                   <div class="row" id="sn_tags">
-                    <label class="bmd-label-floating">Numery seryjne :</label>
-                    <input type="text" id="input-sn_tags" data-role="tagsinput" name="sn_tags" class="form-control col-sm-6">
-                    @if ($errors->has('sn_tags'))
-                      <span class="material-icons form-control-feedback">clear</span>
-                      <span id="sn_tags-error" class="error text-danger" for="input-sn_tags">{{ $errors->first('sn_tags') }}</span>
-                    @endif
+                    {{-- Serial Numbers --}}
+                    <div class="col-sm-6 form-group bmd-form-group {{ $errors->has('sn_tags') ? ' has-danger' : '' }}">
+                      <label class="bmd-label-floating">Numery seryjne :</label>
+                      <input type="text" id="input-sn_tags" data-role="tagsinput" name="sn_tags" class="form-control">
+                      @if ($errors->has('sn_tags'))
+                        <span class="material-icons form-control-feedback">clear</span>
+                        <span id="sn_tags-error" class="error text-danger" for="input-sn_tags">{{ $errors->first('sn_tags') }}</span>
+                      @endif
+                    </div>
                   </div>
                   @if(old('models'))
                   @for ($y = 1; $y < count(old('models')); $y++)
@@ -200,12 +203,15 @@
                     </div>
                   </div>
                   <div class="row" id="sn_tags">
-                    <label class="bmd-label-floating">Numery seryjne :</label>
-                    <input type="text" data-role="tagsinput" name="sn_tags" class="form-control">
-                    @if ($errors->has('sn_tags'))
-                      <span class="material-icons form-control-feedback">clear</span>
-                      <span id="sn_tags-error" class="error text-danger" for="input-sn_tags">{{ $errors->first('sn_tags') }}</span>
-                    @endif
+                    {{-- Serial Numbers --}}
+                    <div class="col-sm-6 form-group bmd-form-group {{ $errors->has('sn_tags') ? ' has-danger' : '' }}">
+                      <label class="bmd-label-floating">Numery seryjne :</label>
+                      <input type="text" id="input-sn_tags" data-role="tagsinput" name="sn_tags" class="form-control">
+                      @if ($errors->has('sn_tags'))
+                        <span class="material-icons form-control-feedback">clear</span>
+                        <span id="sn_tags-error" class="error text-danger" for="input-sn_tags">{{ $errors->first('sn_tags') }}</span>
+                      @endif
+                    </div>
                   </div>
                   @php
                     $dev_counter++;
