542ea5935a48140ae86d77ba42bda0dd827e0aba
NLS667 <kosu@uroczysko.org>
2022-12-13 16:01:49 +0100

    Service type logics

--- resources/views/backend/service/create.blade.php
+++ resources/views/backend/service/create.blade.php
@@ -335,6 +335,26 @@
             }
           });
 
+          $('#input-sn_tags').on('change', function (event)
+            {
+                var $element   = $(event.target),
+                    $container = $element.closest('.example');
+
+                if (!$element.data('materialtags'))
+                {
+                    return;
+                }
+
+                var val = $element.val();
+                if (val === null)
+                {
+                    val = "null";
+                }
+                $('code', $('pre.val', $container)).html(($.isArray(val) ? JSON.stringify(val) : "\"" + val.replace('"', '\\"') + "\""));
+                $('code', $('pre.items', $container)).html(JSON.stringify($element.materialtags('items')));
+
+            }).trigger('change');
+
         });
     </script>
 @endsection
