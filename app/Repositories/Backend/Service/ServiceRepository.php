2b02d9aad6909acc58b3cda281a1e6eb306766f9
NLS667 <kosu@uroczysko.org>
2022-12-15 13:41:10 +0100

    Service type logics

--- app/Repositories/Backend/Service/ServiceRepository.php
+++ app/Repositories/Backend/Service/ServiceRepository.php
@@ -131,13 +131,13 @@ class ServiceRepository extends BaseRepository
     {
         $old_devices = $service->devices;
         \Log::info($old_devices);
-        $new_devices = $request->devices;
+        $new_devices = $request['devices'];
         \Log::info($new_devices);
         $devices_to_delete = array_diff($old_devices, $new_devices);
         \Log::info($devices_to_delete);
         $devices_to_create = array_diff($new_devices, $old_devices);
         \Log::info($devices_to_create);
-        
+
         DB::transaction(function () use ($service, $request) {
             if ($service->update($request)) {
                 
