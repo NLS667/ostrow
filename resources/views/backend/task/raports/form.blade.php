3f754f8355bed7e26066c9983e6ccaeb2a36ef64
NLS667 <kosu@uroczysko.org>
2022-12-19 14:31:56 +0100

    Service type logics

--- resources/views/backend/task/raports/form.blade.php
+++ resources/views/backend/task/raports/form.blade.php
@@ -60,9 +60,10 @@
 			                </div>
 			                <div class="row">
 			                	<div class="col-sm-12">
-			                		{{ $devices }}
+			                		
 			                		@foreach($devices as $device)
 			                		<div class="form-group col-sm-4">
+			                			{{ $device }}
 			                			<input class="form-control" name="model[]" id="input-cmodel" type="text" value="{{ $device->model->name }}" disabled/>
 			                		</div>
 			                		@endforeach
