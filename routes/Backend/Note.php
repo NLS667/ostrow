d5c6225ac6ff476dbb96345397bee58a4afaa1eb
NLS667 <kosu@uroczysko.org>
2023-01-03 11:59:03 +0100

    service form

--- routes/Backend/Note.php
+++ routes/Backend/Note.php
@@ -1,29 +1,30 @@
 <?php
 
 /*
-     * Notes Management
-     */
+ * Notes Management
+ */
+
+Route::group(['middleware' => 'access.routeNeedsPermission:view-client-management',], function () {
     Route::group(['namespace' => 'Note'], function () {
 
+        /*
+         * For notes 
+         */
+        Route::post('note/add-note', [\App\Http\Controllers\Backend\Note\NoteController::class, 'store'])->name('note.add');
+        //Route::post('note/edit', [\App\Http\Controllers\Backend\Note\NoteController::class, 'update'])->name('note.update');
+        //Route::delete('note/delete/{note}', [\App\Http\Controllers\Backend\Note\NoteController::class, 'destroy'])->name('note.destroy');
 
-            /*
-             * For notes 
-             */
-            Route::post('note/add-note', [\App\Http\Controllers\Backend\Note\NoteController::class, 'store'])->name('note.add');
-            //Route::post('note/edit', [\App\Http\Controllers\Backend\Note\NoteController::class, 'update'])->name('note.update');
-            //Route::delete('note/delete/{note}', [\App\Http\Controllers\Backend\Note\NoteController::class, 'destroy'])->name('note.destroy');
-
-            /*
-             * Note CRUD
-             */
-            Route::resource('note', NoteController::class);
+        /*
+         * Note CRUD
+         */
+        Route::resource('note', NoteController::class);
             
+        /*
+         * Specific Note
+         */
+        Route::group(['prefix' => 'note/{note}'], function () {
 
-            /*
-             * Specific Note
-             */
-            Route::group(['prefix' => 'note/{note}'], function () {
-
-            });
         });
+    });
+});
 ?>
