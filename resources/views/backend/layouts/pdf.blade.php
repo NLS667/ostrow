1830c281b48cccfdc9e9b5e4a206610a95c52804
NLS667 <kosu@uroczysko.org>
2023-02-01 11:36:14 +0100

    pdf

--- resources/views/backend/layouts/pdf.blade.php
+++ resources/views/backend/layouts/pdf.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
         <title>Protokół Zadania</title>
-        <link rel="stylesheet" type="text/css" href="{{ base_path().'/resources/assets/sass/plugins/Bootstrap/bootstrap.min.css' }}">
+        <link rel="stylesheet" type="text/css" href="{{ base_path().'/css/bootstrap.css' }}">
     </head>
     <body class="layout-fixed">               
         @yield('content')
