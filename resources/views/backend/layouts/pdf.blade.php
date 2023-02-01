872f51c3c13b0e11f7cc7d40db45a5cd722bf13b
NLS667 <kosu@uroczysko.org>
2023-02-01 13:20:13 +0100

    pdf

--- resources/views/backend/layouts/pdf.blade.php
+++ resources/views/backend/layouts/pdf.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
         <title>Protokół Zadania</title>
-        <link rel="stylesheet" type="text/css" href="{{ public_path('css/bootstrap.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
         <style>
             .page-break {
                 page-break-after: always;
