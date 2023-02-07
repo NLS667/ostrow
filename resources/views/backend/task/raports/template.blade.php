43668af6b71621b32e4baac534c7b085f6cc15ba
NLS667 <kosu@uroczysko.org>
2023-02-07 11:19:22 +0100

    pdf

--- resources/views/backend/task/raports/template.blade.php
+++ resources/views/backend/task/raports/template.blade.php
@@ -10,13 +10,13 @@
 			<div class="col-xs-9">
 				<div class="container">
 					<div class="row">
-						<div class="col-xs-4 border">
+						<div class="col-xs-3 border">
 							Tel. 608 516 632
 						</div>
 						<div class="col-xs-3">
 							info@bio-klim.pl
 						</div>
-						<div class="col-xs-4">
+						<div class="col-xs-3">
 							www.bio-klim.pl
 						</div>
 					</div>
