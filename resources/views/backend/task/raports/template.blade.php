61e7651028601f2f15ebc7f4bb77403005ad8808
NLS667 <kosu@uroczysko.org>
2023-02-07 11:45:01 +0100

    pdf

--- resources/views/backend/task/raports/template.blade.php
+++ resources/views/backend/task/raports/template.blade.php
@@ -10,7 +10,7 @@
 			<div class="col-xs-9">
 				<div class="container border">
 					<div class="row">
-						<div class="col-xs-2">
+						<div class="col-xs-3">
 							Tel. 608 516 632
 						</div>
 						<div class="col-xs-3 border">
