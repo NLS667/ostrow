63acefd21ccb458f1c179ccc310e35ad85c286e0
NLS667 <kosu@uroczysko.org>
2023-02-07 11:04:56 +0100

    pdf

--- resources/views/backend/task/raports/template.blade.php
+++ resources/views/backend/task/raports/template.blade.php
@@ -9,24 +9,20 @@
 			</div>
 			<div class="col-xs-8">
 					<div class="row">
-						<div class="col-xs-1"></div>
-						<div class="col-xs-2">
+						<div class="col-xs-4">
 							Tel. 608 516 632
 						</div>
-						<div class="col-xs-2">
+						<div class="col-xs-3">
 							info@bio-klim.pl
 						</div>
-						<div class="col-xs-2">
+						<div class="col-xs-4">
 							www.bio-klim.pl
 						</div>
-						<div class="col-xs-3"></div>
 					</div>
 					<div class="row">
-						<div class="col-xs-1"></div>
-						<div class="col-xs-8">
+						<div class="col-xs-11">
 							FHU BIO-KLIM Adam Jańcik Prądzyńskiego 30, 63-400 Ostrów Wlkp.
 						</div>
-						<div class="col-xs-2"></div>
 					</div>			
 			</div>
 		</div>
