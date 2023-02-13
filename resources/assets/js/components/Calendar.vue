e2f1312477fb522737bc66bf7ffaa75c62617d11
NLS667 <kosu@uroczysko.org>
2023-02-13 14:44:23 +0100

    kalendarz

--- resources/assets/js/components/Calendar.vue
+++ resources/assets/js/components/Calendar.vue
@@ -44,6 +44,7 @@ export default {
             interactionPlugin,
             listPlugin,
             bootstrapPlugin,
+            resourceTimeGridPlugin,
             CustomViewPlugin
           ],
           headerToolbar : {
