bf7f453aa46c6211b5eb1521416b39e40b94fd26
NLS667 <kosu@uroczysko.org>
2023-02-28 13:46:04 +0100

    Auto-zadania

--- app/Models/Task/Traits/Attribute/TaskAttribute.php
+++ app/Models/Task/Traits/Attribute/TaskAttribute.php
@@ -46,14 +46,14 @@ trait TaskAttribute
     public function getPlanButtonAttribute($class)
     {
         switch ($this->isPlanned) {
-            case true:
+            case 1:
             if (access()->allow('edit-task')) {
                 $name = ($class == '' || $class == 'dropdown-item') ? 'Aktywuj' : '';
 
                 return '<a class="'.$class.'" data-toggle="tooltip" data-placement="top" title="Aktywuj" href="'.route('admin.task.togglePlanned', [$this, 0]).'"><span class="material-icons">event_available</span>'.$name.'</a>';
             }
             break;
-            case false:
+            case 0:
             if (access()->allow('edit-task')) {
                 $name = ($class == '' || $class == 'dropdown-item') ? 'Zaplanuj' : '';
 
