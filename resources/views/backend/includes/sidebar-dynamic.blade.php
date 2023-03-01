08f04ab71b0b612685a6c1bbcd4621e6ceff92b2
NLS667 <kosu@uroczysko.org>
2023-03-01 13:25:20 +0100

    backend appname

--- resources/views/backend/includes/sidebar-dynamic.blade.php
+++ resources/views/backend/includes/sidebar-dynamic.blade.php
@@ -1,8 +1,8 @@
 <!-- Main Sidebar Container -->
 <div class="sidebar" data-color="orange" data-background-color="black" data-image="{{ asset('/img/sidebar-1.jpg') }}">
     <div class="logo">
-        <a href="#" class="simple-text logo-mini">BK</a>
-        <a href="#" class="simple-text logo-normal">{{ config('app.name') }}{{ \Illuminate\Support\Str::substr(config('app.name'), -1) }}</a>
+        <a href="#" class="simple-text logo-mini">{{ \Illuminate\Support\Str::substr(config('app.name'), 0) }}</a>
+        <a href="#" class="simple-text logo-normal">{{ config('app.name') }}</a>
     </div>
     <div class="sidebar-wrapper">
         <ul class="nav">
