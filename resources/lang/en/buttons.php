c216ab660f17c3370d4f656821b6e2ffc1409ad7
NLS667 <kosu@uroczysko.org>
2023-03-06 12:38:21 +0100

    frontend

---
+++ resources/lang/en/buttons.php
@@ -0,0 +1,62 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Buttons Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in buttons throughout the system.
+    | Regardless where it is placed, a button can be listed here so it is easily
+    | found in a intuitive way.
+    |
+    */
+
+    'backend' => [
+        'access' => [
+            'users' => [
+                'activate'           => 'Aktywuj',
+                'change_password'    => 'Zmień Hasło',
+                'clear_session'      => 'Wyczyść sesję',
+                'deactivate'         => 'Deaktywuj',
+                'delete_permanently' => 'Usuń Permanentnie',
+                'login_as'           => 'Zaloguj jako :user',
+                'resend_email'       => 'Wyślij ponownie Confirmation E-mail',
+                'restore_user'       => 'Przywróć Użytkownika',
+            ],
+        ],
+    ],
+
+    'frontend' => [
+        'header' => [
+            'get_started' => 'Zacznij teraz'
+        ],
+    ],
+
+    'emails' => [
+        'auth' => [
+            'confirm_account' => 'Potwierdź założenie Konta',
+            'reset_password'  => 'Resetuj Hasło',
+        ],
+    ],
+
+    'general' => [
+        'cancel'   => 'Anuluj',
+        'continue' => 'Kontynuuj',
+        'preview'  => 'Podgląd',
+
+        'crud' => [
+            'create' => 'Stwórz',
+            'add'    => 'Dodaj',
+            'delete' => 'Usuń',
+            'edit'   => 'Edytuj',
+            'update' => 'Aktualizuj',
+            'view'   => 'Zobacz',
+        ],
+
+        'save' => 'Zapisz',
+        'view' => 'Zobacz',
+        'see_more' => 'Więcej...',
+    ],
+];
---
+++ resources/lang/en/frontpage.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Frontpage Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in front page sections.
+    |
+    */
+
+    'contact' => [
+        'title' => 'Zostań w kontakcie',
+        'fields' => [
+            'name' => [
+                'label' => 'Imię',
+                'placeholder' => 'Twoje Imię',
+            ],
+            'email' => [
+                'label' => 'E-mail',
+                'placeholder' => 'Twój E-mail',
+            ],
+            'message' => [
+                'label' => 'Wiadomość',
+                'placeholder' => 'Opowiedz nam o swoich odczuciach i przemyśleniach...',
+            ],
+        ],  
+        'button' => 'Wyślij teraz',      
+    ],
+];
