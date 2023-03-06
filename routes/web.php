676009ad4fc203f5a6895a9f2d6d7872668b1faa
NLS667 <kosu@uroczysko.org>
2023-03-06 12:51:12 +0100

    frontend

--- routes/web.php
+++ routes/web.php
@@ -62,50 +62,6 @@ Route::group(['middleware' => 'auth'], function () {
     });
 });
 
-
-Route::group(['namespace' => 'Auth', 'as' => 'auth.'], function () {
-
-    /*
-     * These routes require the user to be logged in
-     */
-    Route::group(['middleware' => 'auth'], function () {
-        Route::get('logout', [LoginController::class, 'logout'])->name('logout');
-
-        //For when admin is logged in as user from backend
-        Route::get('logout-as', [LoginController::class, 'logoutAs'])->name('logout-as');
-
-        // Change Password Routes
-        Route::patch('password/change', [ChangePasswordController::class, 'changePassword'])->name('password.change');
-    });
-
-    /*
-     * These routes require no user to be logged in
-     */
-    Route::group(['middleware' => 'guest'], function () {
-        // Authentication Routes
-        //Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
-        Route::post('login', [LoginController::class, 'login'])->name('login');
-
-        // Socialite Routes
-        //Route::get('login/{provider}', 'SocialLoginController@login')->name('social.login');
-
-        // Registration Routes
-        //if (config('access.users.registration')) {
-        //    Route::get('register', 'RegisterController@showRegistrationForm')->name('register');
-        //    Route::post('register', 'RegisterController@register')->name('register');
-        //}
-
-        // Password Reset Routes
-        Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.email');
-        Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
-
-        Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset.form');
-        Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.reset');
-    });
-});
-
-//Route::get('admin', [AdminController::class, 'index'])->name('index');
-
 Route::group(['namespace' => 'Backend','prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'admin'], function () {
     /*
      * These routes need view-backend permission
