5649c96fb647fe3a9029920ec78a2a5a151fcfe7
NLS667 <kosu@uroczysko.org>
2023-03-08 12:51:37 +0100

    frontend

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,58 +10,48 @@
                     <div class="card card-register">
                         <h3 class="card-title">{{ trans('labels.frontend.auth.login_box_title') }}</h3>
                         {{ Form::open(['route' => 'frontend.auth.login', 'class' => 'register-form']) }}
-                        <div class="card-body">
-                            <div class="bmd-form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
-                                <div class="input-group">
+                        <label for="email">{{ trans('labels.frontend.auth.register-user.email') }}</label>
+                            <div class="input-group form-group-no-border{{ $errors->has('email') ? ' has-danger' : '' }}">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                        <i class="nc-icon nc-email-85"></i>
+                                    </span>
+                                </div>
+                                <input id="email" type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="{{ trans('labels.frontend.auth.register-user.email') }}" value="{{ old('email') }}" required>
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <label for="password">{{ trans('labels.frontend.auth.register-user.password') }}</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend form-group-no-border">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text">
-                                            <i class="material-icons">email</i>
+                                            <i class="nc-icon nc-key-25"></i>
                                         </span>
                                     </div>
-                                    <input type="email" name="email" class="form-control" placeholder="Email" value="{{ old('email') }}" required>
                                 </div>
-                                @if ($errors->has('email'))
-                                <div id="email-error" class="error text-danger pl-3" for="email" style="display: block;">
-                                    <strong>{{ $errors->first('email') }}</strong>
-                                </div>
-                                @endif
+                                <input id="password" type="password" name="password" id="password" class="form-control @error('password') is-invalid @enderror" placeholder="{{ trans('labels.frontend.auth.register-user.password') }}" value="{{ !$errors->has('password') ? 'secret' : '' }}" required>
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
-                            <div class="bmd-form-group{{ $errors->has('password') ? ' has-danger' : '' }} mt-3">
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <i class="material-icons">lock_outline</i>
-                                        </span>
-                                    </div>
-                                    <input type="password" name="password" id="password" class="form-control" placeholder="Hasło" value="{{ !$errors->has('password') ? 'secret' : '' }}" required>
-                                </div>
-                                @if ($errors->has('password'))
-                                <div id="password-error" class="error text-danger pl-3" for="password" style="display: block;">
-                                    <strong>{{ $errors->first('password') }}</strong>
-                                </div>
-                                @endif
-                            </div>
-                            <div class="form-check mr-auto ml-3 mt-3">
+                            <div class="form-check">
                                 <label class="form-check-label">
                                     <input class="form-check-input @error('remember') is-invalid @enderror" type="checkbox" name="remember">
                                         <span class="form-check-sign"></span>
                                         <small>{{ trans('labels.frontend.auth.remember_me') }}</small> 
                                 </label>
                             </div>
-                        </div>
-                        <div class="card-footer justify-content-center">
                             {{ Form::submit(trans('labels.frontend.auth.login_button'), ['class' => 'btn btn-primary-blue btn-block btn-round']) }}
-                        </div>
-                    </div>
-                    {{ Form::close() }}
-                    <div class="row">
-                        <div class="col-6">
-                            @if (Route::has('auth.password.reset'))
-                                <a href="{{ route('auth.password.reset') }}" class="text-light">
-                                    <small>Nie pamiętam hasła</small>
-                                </a>
-                            @endif
-                        </div>                        
+                            {{ Form::close() }}
+                            <div class="forgot">
+                                {{ link_to_route('frontend.auth.password.request', trans('labels.frontend.passwords.forgot_password'), [], ['class' => 'btn btn-link btn-primary-blue']) }}
+                            </div>
                     </div>
                 </div>
             </div>
